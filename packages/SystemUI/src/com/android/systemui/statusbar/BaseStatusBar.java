d01c880ae75666bc5f1f9cd1afd97a6eb0ec7b20
dho <dho@dho.im>
2012-08-02 18:13:39 -0400

    toggles

--- packages/SystemUI/src/com/android/systemui/statusbar/BaseStatusBar.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/BaseStatusBar.java
@@ -987,4 +987,9 @@ public abstract class BaseStatusBar extends SystemUI implements
         KeyguardManager km = (KeyguardManager) mContext.getSystemService(Context.KEYGUARD_SERVICE);
         return km.inKeyguardRestrictedInputMode();
     }
+    
+        public boolean isTablet() {
+        return false;
+    }
+    
 }
