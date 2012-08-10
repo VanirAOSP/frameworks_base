1e7ea201ea2739c439fc59c42df90e93b46578bd
Yuhan Xu <yuhanx.xu@intel.com>
2012-08-10 14:31:22 +0800

    [Browser]Full screen progress bar displays on the windows screen during playing vp8 clips or http live streaming clips with html5.
    When quit to window screen from full screen, hide the controller bar.
    
    Change-Id: Ifac3c8f6bc0e9632364ecac85d55b706e9204778
    Author: Yuhan Xu <yuhanx.xu@intel.com>
    Signed-off-by: Yuhan Xu <yuhanx.xu@intel.com>
    Signed-off-by: Shuo Gao <shuo.gao@intel.com>
    Signed-off-by: Bruce Beare <bruce.j.beare@intel.com>
    Signed-off-by: Jack Ren <jack.ren@intel.com>
    Author-tracking-BZ: 40809

--- core/java/android/widget/MediaController.java
+++ core/java/android/widget/MediaController.java
@@ -478,7 +478,8 @@ public class MediaController extends FrameLayout {
         } else if (keyCode == KeyEvent.KEYCODE_VOLUME_DOWN
                 || keyCode == KeyEvent.KEYCODE_VOLUME_UP
                 || keyCode == KeyEvent.KEYCODE_VOLUME_MUTE
-                || keyCode == KeyEvent.KEYCODE_CAMERA) {
+                || keyCode == KeyEvent.KEYCODE_CAMERA
+                || keyCode == KeyEvent.KEYCODE_SEARCH) {
             // don't show the controls for volume adjustment
             return super.dispatchKeyEvent(event);
         } else if (keyCode == KeyEvent.KEYCODE_BACK || keyCode == KeyEvent.KEYCODE_MENU) {
