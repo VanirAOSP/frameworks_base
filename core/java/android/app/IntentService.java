5e13d1c31b16795bebf168b5e6e71c1f131c3d65
Gonçalo Ferreira <monxalo@gmail.com>
2012-08-20 00:31:23 +0100

    IntentService: Fix to run with a lower priority flag.
    Added extra contructor to start IntentService with a diferent priority.
    
    Change-Id: I4224ca56ae7eee2c099140bf0242be33b8690ad8
    Signed-off-by: Gonçalo Ferreira <monxalo@gmail.com>

--- core/java/android/app/IntentService.java
+++ core/java/android/app/IntentService.java
@@ -22,6 +22,7 @@ import android.os.HandlerThread;
 import android.os.IBinder;
 import android.os.Looper;
 import android.os.Message;
+import android.os.Process;
 
 /**
  * IntentService is a base class for {@link Service}s that handle asynchronous
@@ -54,6 +55,7 @@ public abstract class IntentService extends Service {
     private volatile ServiceHandler mServiceHandler;
     private String mName;
     private boolean mRedelivery;
+    private int mPriority = Process.THREAD_PRIORITY_DEFAULT;
 
     private final class ServiceHandler extends Handler {
         public ServiceHandler(Looper looper) {
@@ -75,6 +77,18 @@ public abstract class IntentService extends Service {
     public IntentService(String name) {
         super();
         mName = name;
+    }
+
+    /**
+     * Creates an IntentService with the specified priority.
+     *
+     * @param name Used to name the worker thread, important only for debugging.
+     * @param priority The priority to run the IntentService. The value supplied must be
+     * from {@link android.os.Process}
+     */
+    public IntentService(String name, int priority) {
+        this(name);
+        mPriority = priority;
     }
 
     /**
@@ -104,7 +118,7 @@ public abstract class IntentService extends Service {
         // method that would launch the service & hand off a wakelock.
 
         super.onCreate();
-        HandlerThread thread = new HandlerThread("IntentService[" + mName + "]");
+        HandlerThread thread = new HandlerThread("IntentService[" + mName + "]", mPriority);
         thread.start();
 
         mServiceLooper = thread.getLooper();
