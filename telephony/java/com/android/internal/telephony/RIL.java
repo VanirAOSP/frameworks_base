475576fa07844b7c3050e32d5334016d60a475e3
Hashcode <hashcode0f@gmail.com>
2012-09-24 11:29:48 -0700

    ril: avoid sending null aid string in getIMSI
    A change to how getIMSI works in JB causes null aid strings
    to be sent.  The result for RILs older than v7 is a corrupt
    ril response in SIMRecords::fetchSimRecords.  This corruption
    ends up setting mImsi to null (in SIMRecords) and that causes
    havoc w/ 3g/4g data connections.
    
    This new patch is implemented as an opt-in old ril feature
    
    Change-Id: I6682ea10c8b216e2adc5abffbcc50f3cb6c9d7b8

--- telephony/java/com/android/internal/telephony/RIL.java
+++ telephony/java/com/android/internal/telephony/RIL.java
@@ -893,8 +893,12 @@ public final class RIL extends BaseCommands implements CommandsInterface {
     getIMSIForApp(String aid, Message result) {
         RILRequest rr = RILRequest.obtain(RIL_REQUEST_GET_IMSI, result);
 
-        rr.mp.writeInt(1);
-        rr.mp.writeString(aid);
+        boolean oldRil = needsOldRilFeature("skipnullaid");
+
+        if (aid != null || !oldRil) {
+            rr.mp.writeInt(1);
+            rr.mp.writeString(aid);
+        }
 
         if (RILJ_LOGD) riljLog(rr.serialString() +
                               "> getIMSI: " + requestToString(rr.mRequest)
