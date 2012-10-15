af19d0a3936ae314edc54daaad7e2156c770cd3d
nuclearmistake <nuclearmistake@gmail.com>
2012-10-15 07:39:07 -0400

    TORCH FIX
    Made torch check for a mismatch between torch status and checked-box status1 second after the toggle changes, in addition to checking when a sharedpref change happens (in case it doesn't).
    
    Now, doesn't get stuck in a disabled state, even when screen locked, and in the worst case, takes 2 toggles to affect change in torch status.
    
    Change-Id: I987e77e195da07ec7f026dd2fddaea6dec028ee6
    Signed-off-by: nuclearmistake <nuclearmistake@gmail.com>

--- packages/SystemUI/src/com/android/systemui/statusbar/toggles/TorchToggle.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/toggles/TorchToggle.java
@@ -22,6 +22,7 @@ package com.android.systemui.statusbar.toggles;
 import android.content.Context;
 import android.content.Intent;
 import android.util.Log;
+import android.os.Handler;
 import android.content.SharedPreferences;
 import android.content.SharedPreferences.OnSharedPreferenceChangeListener;
 import android.app.PendingIntent;
@@ -32,13 +33,16 @@ import com.android.systemui.R;
  */
 public class TorchToggle extends Toggle implements
         OnSharedPreferenceChangeListener {
+        
+    private Handler handler = new Handler();
 
     private static final String TAG = "TorchToggle";
 
     public static final String KEY_TORCH_ON = "torch_on";
     public static final String INTENT_TORCH_ON = "com.android.systemui.INTENT_TORCH_ON";
     public static final String INTENT_TORCH_OFF = "com.android.systemui.INTENT_TORCH_OFF";
-
+    private static final String DB_TAG = "TorchDebug";
+    private static final boolean DEBUG = false;
     private boolean mIsTorchOn;
     private Context mContext;
 
@@ -63,6 +67,8 @@ public class TorchToggle extends Toggle implements
 
     @Override
     protected boolean updateInternalToggleState() {
+        if (DEBUG)
+            Log.i(DB_TAG, "updateInternalToggleState() -- mIsTorchOn="+mIsTorchOn);
         mToggle.setChecked(mIsTorchOn);
         if (mToggle.isChecked()) {
             setIcon(R.drawable.toggle_torch);
@@ -75,19 +81,37 @@ public class TorchToggle extends Toggle implements
 
     @Override
     protected void onCheckChanged(boolean isChecked) {
-        mToggle.setEnabled(false); // we've changed torch - let's disable until
-                                   // torch catches up;
-        if (isChecked) {
-            Intent i = new Intent(INTENT_TORCH_ON);
-            i.setAction(INTENT_TORCH_ON);
-            i.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
-            mContext.startActivity(i);
-        } else {
-            Intent i = new Intent(INTENT_TORCH_OFF);
-            i.setAction(INTENT_TORCH_OFF);
-            i.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
-            mContext.startActivity(i);
-        }
+         try
+         {
+            if (DEBUG)
+                Log.i(DB_TAG, "onCheckChanged("+isChecked+") -- mIsTorchOn="+mIsTorchOn);
+            mToggle.setEnabled(false); // we've changed torch - let's disable until
+                                       // torch catches up;
+           if (DEBUG)
+                Log.i(DB_TAG, "mToggle.setEnabled(false)");
+            if (isChecked) {
+                if (DEBUG)
+                    Log.i(DB_TAG, "STARTING INTENT_TORCH_ON");
+                Intent i = new Intent(INTENT_TORCH_ON);
+                i.setAction(INTENT_TORCH_ON);
+                i.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                mContext.startActivity(i);
+            } else {
+                if (DEBUG)
+                    Log.i(DB_TAG, "STARTING INTENT_TORCH_OFF");
+                Intent i = new Intent(INTENT_TORCH_OFF);
+                i.setAction(INTENT_TORCH_OFF);
+                i.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                mContext.startActivity(i);
+            }            
+            handler.removeCallbacks(mMakeSureItReallyHappened); 
+            handler.postDelayed(mMakeSureItReallyHappened, 1000); //give it a second
+                                                                  //then punch it in the face                                                            
+         }
+         catch(Exception e)
+         {
+            Log.e(DB_TAG, "onCheckChanged done exploadeded: "+e.getMessage());
+         }
     }
 
     @Override
@@ -95,13 +119,46 @@ public class TorchToggle extends Toggle implements
         return false;
     }
 
-    public void onSharedPreferenceChanged(SharedPreferences sharedPreferences,
-            String key) {
-        mIsTorchOn = sharedPreferences.getBoolean(KEY_TORCH_ON, false);
-        updateState();
-        if (mToggle.isChecked() == mIsTorchOn) {
-            mToggle.setEnabled(true); // torch status has caught up with toggle
-                                      // - re-enable toggle.
-        }
+    private void BoomRoasted()
+    {
+        try{
+          mIsTorchOn = prefs.getBoolean(KEY_TORCH_ON, false);
+          updateState();
+          if (mToggle.isChecked() == mIsTorchOn) {
+              if (DEBUG)
+                    Log.i(DB_TAG, "TOGGLE AND SHAREDPREF MATCH!!!! BOTH ARE "+mIsTorchOn);
+             mToggle.setEnabled(true); // torch status has caught up with toggle
+                                       // - re-enable toggle.
+           }
+           else 
+           {           
+              if (DEBUG)
+                    Log.e(DB_TAG, "TOGGLE AND SHAREDPREF MISMATCH!!!! mIsTorchOn="+mIsTorchOn+" -- mToggle.isChecked()="+mToggle.isChecked());
+             handler.post(new Runnable() {
+               public void run() {
+                   onCheckChanged(mIsTorchOn);
+               }});
+           }
+         }
+         catch(Exception e)
+         {
+            Log.e("BOOMROASTED", "BoomRoasted done exploadeded: "+e.getMessage());
+         }
     }
+
+    public void onSharedPreferenceChanged(SharedPreferences sharedPreferences,
+            String key) {        
+       if (DEBUG)
+            Log.i(DB_TAG, "onSharedPreferenceChanged fired!");
+        handler.removeCallbacks(mMakeSureItReallyHappened); 
+        BoomRoasted();
+    }
+    
+    private Runnable mMakeSureItReallyHappened = new Runnable() {
+           public void run() {           
+               if (DEBUG)
+                    Log.i(DB_TAG, "DELAYED TEST TO MAKE SURE IT REALLY HAPPENED!");
+               BoomRoasted();
+           }
+        };
 }
