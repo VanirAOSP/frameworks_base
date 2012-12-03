74a37ab9da5e7299e827eec7d1fd4ecd60b46e5a
You Kim <you.kim72@gmail.com>
2012-12-04 00:51:27 +0900

    Remove possible leak in AudioGroup.cpp
    Change-Id: I4d1c16d91fe069ff9cbdc6b4f33651ee5e57fd2d
    Signed-off-by: You Kim <you.kim72@gmail.com>

--- voip/jni/rtp/AudioGroup.cpp
+++ voip/jni/rtp/AudioGroup.cpp
@@ -952,11 +952,6 @@ int add(JNIEnv *env, jobject thiz, jint mode,
         jniThrowNullPointerException(env, "codecSpec");
         return 0;
     }
-    const char *codecSpec = env->GetStringUTFChars(jCodecSpec, NULL);
-    if (!codecSpec) {
-        // Exception already thrown.
-        return 0;
-    }
     socket = dup(socket);
     if (socket == -1) {
         jniThrowException(env, "java/lang/IllegalStateException",
@@ -964,6 +959,11 @@ int add(JNIEnv *env, jobject thiz, jint mode,
         return 0;
     }
 
+    const char *codecSpec = env->GetStringUTFChars(jCodecSpec, NULL);
+    if (!codecSpec) {
+        // Exception already thrown.
+        return 0;
+    }
     // Create audio codec.
     int codecType = -1;
     char codecName[16];
