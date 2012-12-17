de1d0b530fd9622827eb9d68d03832bb8a2cf91e
gmillz <griffinn.millender@gmail.com>
2012-12-16 20:42:18 -0600

    [Framework] Port BatteryBar (2/2)
    Added missing commits Author: kufikugel
    Fix Battery bar 2dp and 3dp graphics glitch
    - unregister observe
    - fix another old old glitch in center mode
    - code cleanup
    Reduce observers in SystemUI: BatteryBarController
    
    - Adds Battery bar with following features
    - location set
    - color
    - aligment
    - thickness
    - animation
    
    - dynamic slim settings
    
    Change-Id: Iacb3e38505b0ab30bb3f9df54231aeb2c79c20f1
    
    Conflicts:
    	core/java/android/provider/Settings.java
    	packages/SystemUI/res/layout-sw600dp/navigation_bar.xml
    	packages/SystemUI/src/com/android/systemui/statusbar/policy/Prefs.java

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -2815,6 +2815,32 @@ public final class Settings {
         public static final String LOCKSCREEN_SEE_THROUGH = "lockscreen_see_through";
 
         /**
+         * Whether to show the battery bar
+         * @hide
+         */
+        public static final String STATUSBAR_BATTERY_BAR = "statusbar_battery_bar";
+
+        /**
+         * @hide
+         */
+        public static final String STATUSBAR_BATTERY_BAR_COLOR = "statusbar_battery_bar_color";
+
+        /**
+         * @hide
+         */
+        public static final String STATUSBAR_BATTERY_BAR_THICKNESS = "statusbar_battery_bar_thickness";
+
+        /**
+         * @hide
+         */
+        public static final String STATUSBAR_BATTERY_BAR_STYLE = "statusbar_battery_bar_style";
+
+        /**
+         * @hide
+         */
+        public static final String STATUSBAR_BATTERY_BAR_ANIMATE = "statusbar_battery_bar_animate";
+
+        /**
          * Whether to show the signal text or signal bars.
          * default: 0
          * 0: show signal bars
