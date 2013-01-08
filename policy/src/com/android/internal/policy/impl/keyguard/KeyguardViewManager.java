46048920f11dfc9eb88b1cf5ee05f7802415105a
Roman Birg <romanbirg@gmail.com>
2013-01-08 17:54:16 -0600

    policy: update(fix?) lockscreen rotation logic
    Change-Id: Ided3c8cd4f52ebd4805189168e13506f74f3443a
    Signed-off-by: Roman Birg <romanbirg@gmail.com>

--- policy/src/com/android/internal/policy/impl/keyguard/KeyguardViewManager.java
+++ policy/src/com/android/internal/policy/impl/keyguard/KeyguardViewManager.java
@@ -183,9 +183,9 @@ public class KeyguardViewManager {
     private boolean shouldEnableScreenRotation() {
         Resources res = mContext.getResources();
         return SystemProperties.getBoolean("lockscreen.rot_override",false)
-                || res.getBoolean(com.android.internal.R.bool.config_enableLockScreenRotation)
-                || Settings.System.getInt(mContext.getContentResolver(),
-                Settings.System.LOCKSCREEN_ALLOW_ROTATION, 0) != 0;
+                || Settings.System.getBoolean(mContext.getContentResolver(),
+                Settings.System.LOCKSCREEN_AUTO_ROTATE, mContext.getResources().getBoolean(
+                com.android.internal.R.bool.config_enableLockScreenRotation));
     }
 
     class ViewManagerHost extends FrameLayout {
