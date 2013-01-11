e35222e76b6e8b778f7d6cca867e5220b9e3adcc
Dirk Rettschlag <dirk.rettschlag@gmail.com>
2013-01-11 12:47:55 +0100

    Lockscreen: fix hiding of initial page hints for landscape
    Change-Id: Ied7a1a0401b7dae9d1900202a51eda34552b4f63
    Signed-off-by: Dirk Rettschlag <dirk.rettschlag@gmail.com>

--- policy/src/com/android/internal/policy/impl/keyguard/KeyguardWidgetCarousel.java
+++ policy/src/com/android/internal/policy/impl/keyguard/KeyguardWidgetCarousel.java
@@ -107,12 +107,12 @@ public class KeyguardWidgetCarousel extends KeyguardWidgetPager {
         for (int i = 0; i < count; i++) {
             boolean inVisibleRange = i >= getNextPage() - 1 && i <= getNextPage() + 1;
             KeyguardWidgetFrame child = getWidgetPageAt(i);
-            if (inVisibleRange) {
-                child.setBackgroundAlpha(KeyguardWidgetFrame.OUTLINE_ALPHA_MULTIPLIER);
-                child.setContentAlpha(1f);
-            } else {
-                child.setBackgroundAlpha(0f);
-                child.setContentAlpha(0f);
+            if (i >= mCurrentPage - 1 && i <= mCurrentPage + 1) {
+                if (!Settings.System.getBoolean(getContext().getContentResolver(),
+                            Settings.System.LOCKSCREEN_HIDE_INITIAL_PAGE_HINTS, false)) {
+                    child.fadeFrame(this, true, KeyguardWidgetFrame.OUTLINE_ALPHA_MULTIPLIER,
+                            CHILDREN_OUTLINE_FADE_IN_DURATION);
+                }
             }
         }
     }
