e32fb16e88505c48ddedc9acd8b2a08ccc12f11c
PrimeDirective <activethrasher00@gmail.com>
2013-01-18 18:49:07 -0500

    quicksetting fixes
    When turning GPS on/off from outside the toggles (ie Settings),
    it would not send updates to the callback implementations.
    
    fix update vibrate/silence not updating
    
    Change-Id: I2f6f7a8a6369fc76c9a771bf21b17ec8495c7c49

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/QuickSettings.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/QuickSettings.java
@@ -294,6 +294,7 @@ class QuickSettings {
                 null, null);
 
         new SettingsObserver(new Handler()).observe();
+        new SoundObserver(new Handler()).observe();
     }
 
     void setBar(PanelBar bar) {
@@ -1196,16 +1197,11 @@ class QuickSettings {
                         boolean gpsEnabled = Settings.Secure.isLocationProviderEnabled(
                                 mContext.getContentResolver(), LocationManager.GPS_PROVIDER);
                         TextView tv = (TextView) view.findViewById(R.id.location_textview);
-                        String newString = state.label;
-                        if ((newString == null) || (newString.equals(""))) {
-                            tv.setText(gpsEnabled ? R.string.quick_settings_gps_on_label
-                                    : R.string.quick_settings_gps_off_label);
-                            tv.setCompoundDrawablesWithIntrinsicBounds(0, gpsEnabled ?
-                                    R.drawable.ic_qs_gps_on : R.drawable.ic_qs_gps_off, 0, 0);
-                        } else {
-                            tv.setText(state.label);
-                            tv.setCompoundDrawablesWithIntrinsicBounds(0, R.drawable.ic_qs_gps_locked, 0, 0);
-                        }
+                        tv.setText(gpsEnabled
+                                ? R.string.quick_settings_gps_on_label
+                                : R.string.quick_settings_gps_off_label);
+                        tv.setCompoundDrawablesWithIntrinsicBounds(0, gpsEnabled ?
+                                R.drawable.ic_qs_gps_on : R.drawable.ic_qs_gps_off, 0, 0);
                         tv.setTextSize(1, mTileTextSize);
                     }
                 });
@@ -1759,6 +1755,27 @@ class QuickSettings {
         @Override
         public void onChange(boolean selfChange) {
             updateSettings();
+        }
+    }
+    
+    class SoundObserver extends ContentObserver {
+        SoundObserver(Handler handler) {
+            super(handler);
+        }
+
+        void observe() {
+            ContentResolver resolver = mContext.getContentResolver();
+            resolver.registerContentObserver(Settings.Global
+                    .getUriFor(Settings.Global.MODE_RINGER),
+                    false, this);
+            mModel.refreshVibrateTile();
+            mModel.refreshSilentTile();
+        }
+
+        @Override
+        public void onChange(boolean selfChange) {
+            mModel.refreshVibrateTile();
+            mModel.refreshSilentTile();
         }
     }
 }
