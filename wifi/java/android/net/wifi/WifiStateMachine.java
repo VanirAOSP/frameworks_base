5abbe7f8204f770948aa38da843c2cdf3bf17b47
Jorge Ruesga <jorge@ruesga.com>
2013-01-23 01:48:37 +0100

    Wi-Fi: Pass default country code to driver
    This patch reverts partially the change http://review.cyanogenmod.org/#/c/28918/8, and
    passing the default country code ("GB") to the driver instead of null.
    
    Change-Id: I7d7115155a0a52261931ffaddaf35f316d095800
    Signed-off-by: Jorge Ruesga <jorge@ruesga.com>

--- wifi/java/android/net/wifi/WifiStateMachine.java
+++ wifi/java/android/net/wifi/WifiStateMachine.java
@@ -1351,7 +1351,15 @@ public class WifiStateMachine extends StateMachine {
         if (countryCode != null && !countryCode.isEmpty()) {
             setCountryCode(countryCode, false);
         } else {
-            //use driver default
+            // On wifi-only devices, some drivers don't find hidden SSIDs unless DRIVER COUNTRY
+            // is called. Use the default country code to ping the driver.
+            ConnectivityManager cm =
+                    (ConnectivityManager)mContext.getSystemService(Context.CONNECTIVITY_SERVICE);
+            if (!cm.isNetworkSupported(ConnectivityManager.TYPE_MOBILE)) {
+                setCountryCode(mCountryCode, false);
+            }
+
+            // In other case, mcc tables from carrier do the trick of starting up the wifi driver
         }
     }
 
@@ -2816,11 +2824,12 @@ public class WifiStateMachine extends StateMachine {
                     break;
                 case CMD_SET_COUNTRY_CODE:
                     String country = (String) message.obj;
-                    if (DBG) log("set country code " + country);
-                    if (mWifiNative.setCountryCode(country.toUpperCase())) {
-                        mCountryCode = country;
+                    String countryCode = country != null ? country.toUpperCase() : null;
+                    if (DBG) log("set country code " + countryCode);
+                    if (mWifiNative.setCountryCode(countryCode)) {
+                        mCountryCode = countryCode;
                     } else {
-                        loge("Failed to set country code " + country);
+                        loge("Failed to set country code " + countryCode);
                     }
                     break;
                 case CMD_SET_FREQUENCY_BAND:
