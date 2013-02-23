0c159fac9e369f13e221f3a03ff2e7ee3e8f84d2
drcmda <drcmda@gmail.com>
2013-02-23 01:04:38 +0000

    Policy: fix keyguard statusbar showing while in ext desktop, fixes it in apps like currents as well
    Change-Id: Id3e4b99d60c33e4050e0e97c365d1ae739f603ec

--- policy/src/com/android/internal/policy/impl/keyguard/KeyguardViewManager.java
+++ policy/src/com/android/internal/policy/impl/keyguard/KeyguardViewManager.java
@@ -186,7 +186,6 @@ public class KeyguardViewManager {
     class ViewManagerHost extends FrameLayout {
         public ViewManagerHost(Context context) {
             super(context);
-            setFitsSystemWindows(true);
         }
 
         @Override
