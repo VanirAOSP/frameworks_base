1339faaa215d58bd8fa9169c2a52ad3b62f20137
DHOMD <DHO@dho.im>
2013-02-24 04:10:32 +0100

    Reconsile profile manger with CM
    Change-Id: Icdc01f1e3e291156d9d8ee887cc0a537c91c3caf

--- core/java/android/app/ProfileManager.java
+++ core/java/android/app/ProfileManager.java
@@ -18,13 +18,19 @@ package android.app;
 
 import java.util.UUID;
 
+import android.annotation.SdkConstant;
+import android.annotation.SdkConstant.SdkConstantType;
 import android.content.Context;
 import android.os.Handler;
 import android.os.IBinder;
 import android.os.ParcelUuid;
 import android.os.RemoteException;
 import android.os.ServiceManager;
+import android.provider.Settings;
 import android.util.Log;
+
+import com.android.internal.R;
+
 
 /**
  * @hide
@@ -38,6 +44,52 @@ public class ProfileManager {
     private static final String TAG = "ProfileManager";
 
     private static final String SYSTEM_PROFILES_ENABLED = "system_profiles_enabled";
+
+    /**
+     * <p>Broadcast Action: A new profile has been selected. This can be triggered by the user
+     * or by calls to the ProfileManagerService / Profile.</p>
+     * @hide
+     */
+    public static final String INTENT_ACTION_PROFILE_SELECTED =
+            "android.intent.action.PROFILE_SELECTED";
+
+    /**
+     * Extra for {@link INTENT_ACTION_PROFILE_SELECTED} and {@link INTENT_ACTION_PROFILE_UPDATED}:
+     * The name of the newly activated or updated profile
+     * @hide
+     */
+    public static final String EXTRA_PROFILE_NAME = "name";
+
+    /**
+     * Extra for {@link INTENT_ACTION_PROFILE_SELECTED} and {@link INTENT_ACTION_PROFILE_UPDATED}:
+     * The string representation of the UUID of the newly activated or updated profile
+     * @hide
+     */
+    public static final String EXTRA_PROFILE_UUID = "uuid";
+
+    /**
+     * Extra for {@link INTENT_ACTION_PROFILE_SELECTED}:
+     * The name of the previously active profile
+     * @hide
+     */
+    public static final String EXTRA_LAST_PROFILE_NAME = "lastName";
+
+    /**
+     * Extra for {@link INTENT_ACTION_PROFILE_SELECTED}:
+     * The string representation of the UUID of the previously active profile
+     * @hide
+     */
+    public static final String EXTRA_LAST_PROFILE_UUID = "uuid";
+
+    /**
+    * <p>Broadcast Action: Current profile has been updated. This is triggered every time the
+    * currently active profile is updated, instead of selected.</p>
+    * <p> For instance, this includes profile updates caused by a locale change, which doesn't
+    * trigger a profile selection, but causes its name to change.</p>
+    * @hide
+    */
+    public static final String INTENT_ACTION_PROFILE_UPDATED =
+            "android.intent.action.PROFILE_UPDATED";
 
     /**
      * Activity Action: Shows a profile picker.
@@ -156,32 +208,50 @@ public class ProfileManager {
     /** @hide */
     ProfileManager(Context context, Handler handler) {
         mContext = context;
+        mEmptyProfile = new Profile("EmptyProfile");
     }
 
     @Deprecated
     public void setActiveProfile(String profileName) {
-        try {
-            getService().setActiveProfileByName(profileName);
-        } catch (RemoteException e) {
-            Log.e(TAG, e.getLocalizedMessage(), e);
+        if (Settings.System.getInt(mContext.getContentResolver(),
+                SYSTEM_PROFILES_ENABLED, 1) == 1) {
+            // Profiles are enabled, return active profile
+            try {
+                getService().setActiveProfileByName(profileName);
+            } catch (RemoteException e) {
+                Log.e(TAG, e.getLocalizedMessage(), e);
+            }
         }
     }
 
     public void setActiveProfile(UUID profileUuid) {
-        try {
-            getService().setActiveProfile(new ParcelUuid(profileUuid));
-        } catch (RemoteException e) {
-            Log.e(TAG, e.getLocalizedMessage(), e);
+        if (Settings.System.getInt(mContext.getContentResolver(),
+                SYSTEM_PROFILES_ENABLED, 1) == 1) {
+            // Profiles are enabled, return active profile
+            try {
+                getService().setActiveProfile(new ParcelUuid(profileUuid));
+            } catch (RemoteException e) {
+                Log.e(TAG, e.getLocalizedMessage(), e);
+            }
         }
     }
 
     public Profile getActiveProfile() {
-        try {
-            return getService().getActiveProfile();
-        } catch (RemoteException e) {
-            Log.e(TAG, e.getLocalizedMessage(), e);
+        if (Settings.System.getInt(mContext.getContentResolver(),
+                SYSTEM_PROFILES_ENABLED, 1) == 1) {
+            // Profiles are enabled, return active profile
+            try {
+                return getService().getActiveProfile();
+            } catch (RemoteException e) {
+                Log.e(TAG, e.getLocalizedMessage(), e);
+            }
+            return null;
+
+        } else {
+            // Profiles are not enabled, return the empty profile
+            return mEmptyProfile;
         }
-        return null;
+
     }
 
     /** @hide */
