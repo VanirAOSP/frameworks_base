51f5c737f6968e0b877f612531b1e96befc87b0a
Nate <nategantt@gmail.com>
2013-02-25 20:39:33 -0500

    Swap volume buttons when device is rotated
    Feel free to merge.
    Original work by Dávid László of CM
    Change-Id: Ida9bce62aa293b49ea9076ff250afc4be0007d37

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -2396,6 +2396,12 @@ public final class Settings {
         public static final String LOCKSCREEN_DISABLED = "lockscreen.disabled";
 
         /**
+          * Swap volume buttons when the screen is rotated by 90 or 180 degrees
+          * @hide
+          */
+         public static final String SWAP_VOLUME_KEYS_BY_ROTATE = "swap_volume_keys_by_rotate";
+
+        /**
          * Weather to minimize lockscreen challenge on screen turned on
          * @hide
          */
