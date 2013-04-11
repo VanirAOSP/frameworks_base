daffc81328b3bbda317ac29a51e91376e8fa12bd
PrimeDirective <activethrasher00@gmail.com>
2013-04-10 21:32:55 -0400

    Revert "Fix a programmatic smooth scroll bug"
    This reverts commit 463996c42e4a388561efe9ccf86ec61aa201d828.
    
    Change-Id: I0406e72f1872b1d51876bd1702b9131a90174823

--- core/java/android/widget/AbsListView.java
+++ core/java/android/widget/AbsListView.java
@@ -4441,7 +4441,7 @@ public abstract class AbsListView extends AdapterView<ListAdapter> implements Te
                     return;
                 }
 
-                if (lastPos == mLastSeenPos && getChildCount() > 1) {
+                if (lastPos == mLastSeenPos) {
                     // No new views, let things keep going.
                     postOnAnimation(this);
                     return;
@@ -4474,7 +4474,7 @@ public abstract class AbsListView extends AdapterView<ListAdapter> implements Te
                 }
                 final int nextPos = firstPos + nextViewIndex;
 
-                if (nextPos == mLastSeenPos && getChildCount() > 1) {
+                if (nextPos == mLastSeenPos) {
                     // No new views, let things keep going.
                     postOnAnimation(this);
                     return;
@@ -4500,7 +4500,7 @@ public abstract class AbsListView extends AdapterView<ListAdapter> implements Te
             }
 
             case MOVE_UP_POS: {
-                if (firstPos == mLastSeenPos && getChildCount() > 1) {
+                if (firstPos == mLastSeenPos) {
                     // No new views, let things keep going.
                     postOnAnimation(this);
                     return;
@@ -4531,7 +4531,7 @@ public abstract class AbsListView extends AdapterView<ListAdapter> implements Te
                 }
                 final int lastPos = firstPos + lastViewIndex;
 
-                if (lastPos == mLastSeenPos && getChildCount() > 1) {
+                if (lastPos == mLastSeenPos) {
                     // No new views, let things keep going.
                     postOnAnimation(this);
                     return;
@@ -4557,7 +4557,7 @@ public abstract class AbsListView extends AdapterView<ListAdapter> implements Te
             }
 
             case MOVE_OFFSET: {
-                if (mLastSeenPos == firstPos && getChildCount() > 1) {
+                if (mLastSeenPos == firstPos) {
                     // No new views, let things keep going.
                     postOnAnimation(this);
                     return;
