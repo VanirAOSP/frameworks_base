c7fc51375f1541ee78d4d796afc6eae1656f1da9
Der-Schubi <schubi@erlangen.ccc.de>
2013-06-17 03:13:53 +0200

    Halo: Add master switch to disable Halo (1/2)
    -Port master switch from @Der-Schubi.
    -Fixed Halo not showed at first notifiction pulldown.
    
    Change-Id: I4ab22fe45353a00232617f0e835c88b15b2c6981
    Signed-off-by: Dan Trifan <jimsth@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -2772,6 +2772,12 @@ public final class Settings {
          */
         public static final String PIE_CONTROLS = "pie_controls";
 
+       /**
+         * HALO enabled, should default to 0 (HALO is disabled)
+         * @hide
+         */
+        public static final String HALO_ENABLED = "halo_enabled";
+
         /**
          * HALO, should default to 0 (no, do not show)
          * @hide
