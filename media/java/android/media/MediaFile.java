VanirAOSP/frameworks_base

commit 82a6b49239fe5d9ce54e30536dfbb3ad8f9b036a

Squashed commit of audio changes from CodeAurora
MediaFile: Add DIVX and MPEG2TS file format

Change-Id: I9f8cfbe8138c6d3d0d824bff2542b91d72a06b82

audio: Define ANC headset and proxy device

Change-Id: I681d73433d874ec66ec0b1f1f39869008d7d7d0d
(cherry picked from commit 1e6c410eadfc510a9f116bb3da142214fe761225)

audio: add support for QCOM WiFi display

- Define QCOM WFD intents
- Register and handle WFD intends in AudioSystem

Change-Id: I1e5a73d4aa5595bed44d24652647c12b7ef4ae1f

audio: add support for QCOM audio formats

- Add support for EVRC, QCELP, and WMA formats

Change-Id: If3676b88fa287b484ef8616c0a6b67177b61a720

PhoneWindowManager: Changes for WFD.

- WFD now sends WIFI_DISPLAY_VIDEO intent to set UI orientation.
- PhoneWindowManager to listen for WFD intents.
- Display on WFD should be same as seen on the HDMI, so set the
  orientation to HDMI orientation

Change-Id: I638adaa906e2151b1c6ae496343714fe828f5dbd

audio: add support for QCOM vocoders

- Add support for EVRC,QCELP and other
  QCOM vocoders

Change-Id: I9b2a18fe7d01a9198eb470e94e7176b7a6da2c7c

media/java/android/media/MediaFile.java
@@ -1,5 +1,6 @@
 /*
  * Copyright (C) 2007 The Android Open Source Project
+ * Copyright (c) 2012, The Linux Foundation. All rights reserved.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -47,16 +48,27 @@ public class MediaFile {
     public static final int FILE_TYPE_AAC     = 8;
     public static final int FILE_TYPE_MKA     = 9;
     public static final int FILE_TYPE_FLAC    = 10;
+    public static final int FILE_TYPE_3GPA    = 11;
+    public static final int FILE_TYPE_AC3     = 12;
+    public static final int FILE_TYPE_QCP     = 13;
+    public static final int FILE_TYPE_WEBMA   = 14;
+    public static final int FILE_TYPE_PCM     = 15;
+    public static final int FILE_TYPE_EC3     = 16;
     private static final int FIRST_AUDIO_FILE_TYPE = FILE_TYPE_MP3;
-    private static final int LAST_AUDIO_FILE_TYPE = FILE_TYPE_FLAC;
+    private static final int LAST_AUDIO_FILE_TYPE = FILE_TYPE_EC3;
+
+    // More audio file types
+    public static final int FILE_TYPE_DTS   = 300;
+    private static final int FIRST_AUDIO_FILE_TYPE2 = FILE_TYPE_DTS;
+    private static final int LAST_AUDIO_FILE_TYPE2 = FILE_TYPE_DTS;
 
     // MIDI file types
-    public static final int FILE_TYPE_MID     = 11;
-    public static final int FILE_TYPE_SMF     = 12;
-    public static final int FILE_TYPE_IMY     = 13;
+    public static final int FILE_TYPE_MID     = 16;
+    public static final int FILE_TYPE_SMF     = 17;
+    public static final int FILE_TYPE_IMY     = 18;
     private static final int FIRST_MIDI_FILE_TYPE = FILE_TYPE_MID;
     private static final int LAST_MIDI_FILE_TYPE = FILE_TYPE_IMY;
-   
+
     // Video file types
     public static final int FILE_TYPE_MP4     = 21;
     public static final int FILE_TYPE_M4V     = 22;
@@ -68,9 +80,10 @@ public class MediaFile {
     public static final int FILE_TYPE_MP2TS   = 28;
     public static final int FILE_TYPE_AVI     = 29;
     public static final int FILE_TYPE_WEBM    = 30;
+    public static final int FILE_TYPE_DIVX    = 31;
     private static final int FIRST_VIDEO_FILE_TYPE = FILE_TYPE_MP4;
-    private static final int LAST_VIDEO_FILE_TYPE = FILE_TYPE_WEBM;
-    
+    private static final int LAST_VIDEO_FILE_TYPE = FILE_TYPE_DIVX;
+
     // More video file types
     public static final int FILE_TYPE_MP2PS   = 200;
     private static final int FIRST_VIDEO_FILE_TYPE2 = FILE_TYPE_MP2PS;
@@ -85,7 +98,7 @@ public class MediaFile {
     public static final int FILE_TYPE_WEBP    = 36;
     private static final int FIRST_IMAGE_FILE_TYPE = FILE_TYPE_JPEG;
     private static final int LAST_IMAGE_FILE_TYPE = FILE_TYPE_WEBP;
-   
+
     // Playlist file types
     public static final int FILE_TYPE_M3U      = 41;
     public static final int FILE_TYPE_PLS      = 42;
@@ -109,17 +122,17 @@ public class MediaFile {
     public static final int FILE_TYPE_MS_EXCEL      = 105;
     public static final int FILE_TYPE_MS_POWERPOINT = 106;
     public static final int FILE_TYPE_ZIP           = 107;
-    
+
     public static class MediaFileType {
         public final int fileType;
         public final String mimeType;
-        
+
         MediaFileType(int fileType, String mimeType) {
             this.fileType = fileType;
             this.mimeType = mimeType;
         }
     }
-    
+
     private static final HashMap<String, MediaFileType> sFileTypeMap
             = new HashMap<String, MediaFileType>();
     private static final HashMap<String, Integer> sMimeTypeMap
@@ -175,18 +188,22 @@ public class MediaFile {
         addFileType("MPGA", FILE_TYPE_MP3, "audio/mpeg", MtpConstants.FORMAT_MP3);
         addFileType("M4A", FILE_TYPE_M4A, "audio/mp4", MtpConstants.FORMAT_MPEG);
         addFileType("WAV", FILE_TYPE_WAV, "audio/x-wav", MtpConstants.FORMAT_WAV);
+        addFileType("WAV", FILE_TYPE_PCM, "audio/wav");
         addFileType("AMR", FILE_TYPE_AMR, "audio/amr");
         addFileType("AWB", FILE_TYPE_AWB, "audio/amr-wb");
+        addFileType("DIVX", FILE_TYPE_DIVX, "video/divx");
         if (isWMAEnabled()) {
             addFileType("WMA", FILE_TYPE_WMA, "audio/x-ms-wma", MtpConstants.FORMAT_WMA);
         }
+        addFileType("QCP", FILE_TYPE_QCP, "audio/qcelp");
         addFileType("OGG", FILE_TYPE_OGG, "audio/ogg", MtpConstants.FORMAT_OGG);
         addFileType("OGG", FILE_TYPE_OGG, "application/ogg", MtpConstants.FORMAT_OGG);
+        addFileType("OGA", FILE_TYPE_OGG, "audio/ogg", MtpConstants.FORMAT_OGG);
         addFileType("OGA", FILE_TYPE_OGG, "application/ogg", MtpConstants.FORMAT_OGG);
         addFileType("AAC", FILE_TYPE_AAC, "audio/aac", MtpConstants.FORMAT_AAC);
         addFileType("AAC", FILE_TYPE_AAC, "audio/aac-adts", MtpConstants.FORMAT_AAC);
         addFileType("MKA", FILE_TYPE_MKA, "audio/x-matroska");
- 
+
         addFileType("MID", FILE_TYPE_MID, "audio/midi");
         addFileType("MIDI", FILE_TYPE_MID, "audio/midi");
         addFileType("XMF", FILE_TYPE_MID, "audio/midi");
@@ -196,7 +213,7 @@ public class MediaFile {
         addFileType("RTX", FILE_TYPE_MID, "audio/midi");
         addFileType("OTA", FILE_TYPE_MID, "audio/midi");
         addFileType("MXMF", FILE_TYPE_MID, "audio/midi");
-        
+
         addFileType("MPEG", FILE_TYPE_MP4, "video/mpeg", MtpConstants.FORMAT_MPEG);
         addFileType("MPG", FILE_TYPE_MP4, "video/mpeg", MtpConstants.FORMAT_MPEG);
         addFileType("MP4", FILE_TYPE_MP4, "video/mp4", MtpConstants.FORMAT_MPEG);
@@ -208,6 +225,8 @@ public class MediaFile {
         addFileType("MKV", FILE_TYPE_MKV, "video/x-matroska");
         addFileType("WEBM", FILE_TYPE_WEBM, "video/webm");
         addFileType("TS", FILE_TYPE_MP2TS, "video/mp2ts");
+        addFileType("MPG", FILE_TYPE_MP2TS, "video/mp2ts");
+
         addFileType("AVI", FILE_TYPE_AVI, "video/avi");
 
         if (isWMVEnabled()) {
@@ -222,7 +241,7 @@ public class MediaFile {
         addFileType("BMP", FILE_TYPE_BMP, "image/x-ms-bmp", MtpConstants.FORMAT_BMP);
         addFileType("WBMP", FILE_TYPE_WBMP, "image/vnd.wap.wbmp");
         addFileType("WEBP", FILE_TYPE_WEBP, "image/webp");
- 
+
         addFileType("M3U", FILE_TYPE_M3U, "audio/x-mpegurl", MtpConstants.FORMAT_M3U_PLAYLIST);
         addFileType("M3U", FILE_TYPE_M3U, "application/x-mpegurl", MtpConstants.FORMAT_M3U_PLAYLIST);
         addFileType("PLS", FILE_TYPE_PLS, "audio/x-scpls", MtpConstants.FORMAT_PLS_PLAYLIST);
@@ -250,7 +269,9 @@ public class MediaFile {
         return ((fileType >= FIRST_AUDIO_FILE_TYPE &&
                 fileType <= LAST_AUDIO_FILE_TYPE) ||
                 (fileType >= FIRST_MIDI_FILE_TYPE &&
-                fileType <= LAST_MIDI_FILE_TYPE));
+                fileType <= LAST_MIDI_FILE_TYPE) ||
+                (fileType >= FIRST_AUDIO_FILE_TYPE2 &&
+                fileType <= LAST_AUDIO_FILE_TYPE2));
     }
 
     public static boolean isVideoFileType(int fileType) {

core/java/android/content/Intent.java
@@ -2154,6 +2154,31 @@ public class Intent implements Parcelable, Cloneable {
             "android.intent.action.HEADSET_PLUG";
 
     /**
+     * Broadcast Action: WiFi Display audio is enabled or disabled
+     *
+     * <p>The intent will have the following extra values:
+     * <ul>
+     *   <li><em>state</em> - 0 for disabled, 1 for enabled. </li>
+     * </ul>
+     * @hide
+     */
+    public static final String ACTION_WIFI_DISPLAY_AUDIO =
+            "qualcomm.intent.action.WIFI_DISPLAY_AUDIO";
+
+    /**
+     * Broadcast Action: WiFi Display video is enabled or disabled
+     *
+     * <p>The intent will have the following extra values:
+     * <ul>
+     *   <li><em>state</em> - 0 for disabled, 1 for enabled. </li>
+     * </ul>
+     * @hide
+     */
+
+    public static final String ACTION_WIFI_DISPLAY_VIDEO =
+            "qualcomm.intent.action.WIFI_DISPLAY_VIDEO";
+
+    /**
      * Broadcast Action: An analog audio speaker/headset plugged in or unplugged.
      *
      * <p>The intent will have the following extra values:

core/jni/android_media_AudioRecord.cpp
@@ -49,6 +49,11 @@ struct fields_t {
     jmethodID postNativeEventInJava; //... event post callback method
     int       PCM16;                 //...  format constants
     int       PCM8;                  //...  format constants
+    int       AMRNB;                 //...  format constants
+    int       AMRWB;                 //...  format constants
+    int       EVRC;                  //...  format constants
+    int       EVRCB;                 //...  format constants
+    int       EVRCWB;                //...  format constants
     jfieldID  nativeRecorderInJavaObj; // provides access to the C++ AudioRecord object
     jfieldID  nativeCallbackCookie;    // provides access to the AudioRecord callback data
 };
@@ -162,6 +167,24 @@ static sp<AudioRecord> setAudioRecord(JNIEnv* env, jobject thiz, const sp<AudioR
     env->SetIntField(thiz, javaAudioRecordFields.nativeRecorderInJavaObj, (int)ar.get());
     return old;
 }
+int getformatrec(int audioformat)
+{
+    if(audioformat==javaAudioRecordFields.PCM16)
+        return AUDIO_FORMAT_PCM_16_BIT;
+#ifdef QCOM_HARDWARE
+    else if(audioformat==javaAudioRecordFields.AMRNB)
+        return AUDIO_FORMAT_AMR_NB;
+    else if(audioformat==javaAudioRecordFields.AMRWB)
+        return AUDIO_FORMAT_AMR_WB;
+    else if(audioformat==javaAudioRecordFields.EVRC)
+        return AUDIO_FORMAT_EVRC;
+    else if(audioformat==javaAudioRecordFields.EVRCB)
+        return AUDIO_FORMAT_EVRCB;
+    else if(audioformat==javaAudioRecordFields.EVRCWB)
+        return AUDIO_FORMAT_EVRCWB;
+#endif
+    return AUDIO_FORMAT_PCM_8_BIT;
+}
 
 // ----------------------------------------------------------------------------
 static int
@@ -181,14 +204,27 @@ android_media_AudioRecord_setup(JNIEnv *env, jobject thiz, jobject weak_this,
 
     // compare the format against the Java constants
     if ((audioFormat != javaAudioRecordFields.PCM16)
+#ifdef QCOM_HARDWARE
+        && (audioFormat != javaAudioRecordFields.AMRNB)
+        && (audioFormat != javaAudioRecordFields.AMRWB)
+        && (audioFormat != javaAudioRecordFields.EVRC)
+        && (audioFormat != javaAudioRecordFields.EVRCB)
+#endif
         && (audioFormat != javaAudioRecordFields.PCM8)) {
         ALOGE("Error creating AudioRecord: unsupported audio format.");
         return AUDIORECORD_ERROR_SETUP_INVALIDFORMAT;
     }
-
-    int bytesPerSample = audioFormat==javaAudioRecordFields.PCM16 ? 2 : 1;
-    audio_format_t format = audioFormat==javaAudioRecordFields.PCM16 ?
-            AUDIO_FORMAT_PCM_16_BIT : AUDIO_FORMAT_PCM_8_BIT;
+    int bytesPerSample;
+    if(audioFormat == javaAudioRecordFields.PCM8)
+        bytesPerSample = 1;
+#ifdef QCOM_HARDWARE
+    else if((audioFormat == javaAudioRecordFields.AMRWB) &&
+            ((uint32_t)source != AUDIO_SOURCE_VOICE_COMMUNICATION))
+        bytesPerSample = 61;
+#endif
+    else
+        bytesPerSample = 2;
+    audio_format_t format = (audio_format_t)getformatrec(audioFormat);
 
     if (buffSizeInBytes == 0) {
          ALOGE("Error creating AudioRecord: frameCount is 0.");
@@ -514,9 +550,8 @@ static jint android_media_AudioRecord_get_min_buff_size(JNIEnv *env,  jobject th
     int frameCount = 0;
     status_t result = AudioRecord::getMinFrameCount(&frameCount,
             sampleRateInHertz,
-            (audioFormat == javaAudioRecordFields.PCM16 ?
-                AUDIO_FORMAT_PCM_16_BIT : AUDIO_FORMAT_PCM_8_BIT),
-            audio_channel_in_mask_from_count(nbChannels));
+            (audio_format_t)getformatrec(audioFormat),
+            nbChannels);
 
     if (result == BAD_VALUE) {
         return 0;
@@ -524,7 +559,17 @@ static jint android_media_AudioRecord_get_min_buff_size(JNIEnv *env,  jobject th
     if (result != NO_ERROR) {
         return -1;
     }
-    return frameCount * nbChannels * (audioFormat == javaAudioRecordFields.PCM16 ? 2 : 1);
+    int bytesPerSample;
+    if(audioFormat == javaAudioRecordFields.PCM8)
+        bytesPerSample = 1;
+#ifdef QCOM_HARDWARE
+    else if(audioFormat == javaAudioRecordFields.AMRWB)
+        bytesPerSample = 61;
+#endif
+    else
+        bytesPerSample = 2;
+
+    return frameCount * nbChannels * bytesPerSample;
 }
 
 
@@ -558,6 +603,11 @@ static JNINativeMethod gMethods[] = {
 #define JAVA_POSTEVENT_CALLBACK_NAME  "postEventFromNative"
 #define JAVA_CONST_PCM16_NAME         "ENCODING_PCM_16BIT"
 #define JAVA_CONST_PCM8_NAME          "ENCODING_PCM_8BIT"
+#define JAVA_CONST_AMRNB_NAME         "ENCODING_AMRNB"
+#define JAVA_CONST_AMRWB_NAME         "ENCODING_AMRWB"
+#define JAVA_CONST_EVRC_NAME          "ENCODING_EVRC"
+#define JAVA_CONST_EVRCB_NAME         "ENCODING_EVRCB"
+#define JAVA_CONST_EVRCWB_NAME        "ENCODING_EVRCWB"
 #define JAVA_NATIVERECORDERINJAVAOBJ_FIELD_NAME  "mNativeRecorderInJavaObj"
 #define JAVA_NATIVECALLBACKINFO_FIELD_NAME       "mNativeCallbackCookie"
 
@@ -621,7 +671,22 @@ int register_android_media_AudioRecord(JNIEnv *env)
                 JAVA_CONST_PCM16_NAME, &(javaAudioRecordFields.PCM16))
            || !android_media_getIntConstantFromClass(env, audioFormatClass,
                 JAVA_AUDIOFORMAT_CLASS_NAME,
-                JAVA_CONST_PCM8_NAME, &(javaAudioRecordFields.PCM8)) ) {
+                JAVA_CONST_PCM8_NAME, &(javaAudioRecordFields.PCM8))
+           || !android_media_getIntConstantFromClass(env, audioFormatClass,
+                JAVA_AUDIOFORMAT_CLASS_NAME,
+                JAVA_CONST_AMRNB_NAME, &(javaAudioRecordFields.AMRNB))
+           || !android_media_getIntConstantFromClass(env, audioFormatClass,
+                JAVA_AUDIOFORMAT_CLASS_NAME,
+                JAVA_CONST_AMRWB_NAME, &(javaAudioRecordFields.AMRWB))
+           || !android_media_getIntConstantFromClass(env, audioFormatClass,
+                JAVA_AUDIOFORMAT_CLASS_NAME,
+                JAVA_CONST_EVRC_NAME, &(javaAudioRecordFields.EVRC))
+           || !android_media_getIntConstantFromClass(env, audioFormatClass,
+                JAVA_AUDIOFORMAT_CLASS_NAME,
+                JAVA_CONST_EVRCB_NAME, &(javaAudioRecordFields.EVRCB))
+           || !android_media_getIntConstantFromClass(env, audioFormatClass,
+                JAVA_AUDIOFORMAT_CLASS_NAME,
+                JAVA_CONST_EVRCWB_NAME, &(javaAudioRecordFields.EVRCWB))) {
         // error log performed in getIntConstantFromClass()
         return -1;
     }

core/jni/android_media_AudioTrack.cpp
@@ -1,5 +1,6 @@
 /*
  * Copyright (C) 2008 The Android Open Source Project
+ * Copyright (c) 2012, The Linux Foundation. All rights reserved.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -49,6 +50,19 @@ struct fields_t {
     jmethodID postNativeEventInJava; //... event post callback method
     int       PCM16;                 //...  format constants
     int       PCM8;                  //...  format constants
+    int       AMRNB;                 //...  format constants
+    int       AMRWB;                 //...  format constants
+    int       EVRC;                  //...  format constants
+    int       EVRCB;                 //...  format constants
+    int       EVRCWB;                //...  format constants
+    int       STREAM_VOICE_CALL;     //...  stream type constants
+    int       STREAM_SYSTEM;         //...  stream type constants
+    int       STREAM_RING;           //...  stream type constants
+    int       STREAM_MUSIC;          //...  stream type constants
+    int       STREAM_ALARM;          //...  stream type constants
+    int       STREAM_NOTIFICATION;   //...  stream type constants
+    int       STREAM_BLUETOOTH_SCO;  //...  stream type constants
+    int       STREAM_DTMF;           //...  stream type constants
     int       MODE_STREAM;           //...  memory mode
     int       MODE_STATIC;           //...  memory mode
     jfieldID  nativeTrackInJavaObj;  // stores in Java the native AudioTrack object
@@ -199,6 +213,25 @@ static sp<AudioTrack> setAudioTrack(JNIEnv* env, jobject thiz, const sp<AudioTra
 }
 
 // ----------------------------------------------------------------------------
+int getformat(int audioformat)
+{
+    if(audioformat==javaAudioTrackFields.PCM16)
+        return AUDIO_FORMAT_PCM_16_BIT;
+#ifdef QCOM_HARDWARE
+    else if(audioformat==javaAudioTrackFields.AMRNB)
+        return AUDIO_FORMAT_AMR_NB;
+    else if(audioformat==javaAudioTrackFields.AMRWB)
+        return AUDIO_FORMAT_AMR_WB;
+    else if(audioformat==javaAudioTrackFields.EVRC)
+        return AUDIO_FORMAT_EVRC;
+    else if(audioformat==javaAudioTrackFields.EVRCB)
+        return AUDIO_FORMAT_EVRCB;
+    else if(audioformat==javaAudioTrackFields.EVRCWB)
+        return AUDIO_FORMAT_EVRCWB;
+#endif
+    return AUDIO_FORMAT_PCM_8_BIT;
+}
+
 static int
 android_media_AudioTrack_native_setup(JNIEnv *env, jobject thiz, jobject weak_this,
         jint streamType, jint sampleRateInHertz, jint javaChannelMask,
@@ -249,7 +282,15 @@ android_media_AudioTrack_native_setup(JNIEnv *env, jobject thiz, jobject weak_th
 
     // check the format.
     // This function was called from Java, so we compare the format against the Java constants
-    if ((audioFormat != javaAudioTrackFields.PCM16) && (audioFormat != javaAudioTrackFields.PCM8)) {
+    if ((audioFormat != javaAudioTrackFields.PCM16)
+#ifdef QCOM_HARDWARE
+        && (audioFormat != javaAudioTrackFields.AMRNB)
+        && (audioFormat != javaAudioTrackFields.AMRWB)
+        && (audioFormat != javaAudioTrackFields.EVRC)
+        && (audioFormat != javaAudioTrackFields.EVRCB)
+        && (audioFormat != javaAudioTrackFields.EVRCWB)
+#endif
+        && (audioFormat != javaAudioTrackFields.PCM8)) {
         ALOGE("Error creating AudioTrack: unsupported audio format.");
         return AUDIOTRACK_ERROR_SETUP_INVALIDFORMAT;
     }
@@ -268,9 +309,12 @@ android_media_AudioTrack_native_setup(JNIEnv *env, jobject thiz, jobject weak_th
     }
 
     // compute the frame count
-    int bytesPerSample = audioFormat == javaAudioTrackFields.PCM16 ? 2 : 1;
-    audio_format_t format = audioFormat == javaAudioTrackFields.PCM16 ?
-            AUDIO_FORMAT_PCM_16_BIT : AUDIO_FORMAT_PCM_8_BIT;
+    int bytesPerSample;
+    if(audioFormat == javaAudioTrackFields.PCM8)
+        bytesPerSample = 1;
+    else
+        bytesPerSample = 2;
+    audio_format_t format = (audio_format_t)getformat(audioFormat);
     int frameCount = buffSizeInBytes / (nbChannels * bytesPerSample);
 
     jclass clazz = env->GetObjectClass(thiz);
@@ -516,7 +560,16 @@ jint writeToTrack(const sp<AudioTrack>& track, jint audioFormat, jbyte* data,
     if (track->sharedBuffer() == 0) {
         written = track->write(data + offsetInBytes, sizeInBytes);
     } else {
+#ifdef QCOM_HARDWARE
+        if ((audioFormat == javaAudioTrackFields.PCM16)
+        || (audioFormat == javaAudioTrackFields.AMRNB)
+        || (audioFormat == javaAudioTrackFields.AMRWB)
+        || (audioFormat == javaAudioTrackFields.EVRC)
+        || (audioFormat == javaAudioTrackFields.EVRCB)
+        || (audioFormat == javaAudioTrackFields.EVRCWB)) {
+#else
         if (audioFormat == javaAudioTrackFields.PCM16) {
+#endif
             // writing to shared memory, check for capacity
             if ((size_t)sizeInBytes > track->sharedBuffer()->size()) {
                 sizeInBytes = track->sharedBuffer()->size();
@@ -791,7 +844,7 @@ static jint android_media_AudioTrack_get_min_buff_size(JNIEnv *env,  jobject thi
             sampleRateInHertz) != NO_ERROR) {
         return -1;
     }
-    return frameCount * nbChannels * (audioFormat == javaAudioTrackFields.PCM16 ? 2 : 1);
+    return frameCount * nbChannels * (audioFormat == javaAudioTrackFields.PCM8 ? 1 : 2);
 }
 
 // ----------------------------------------------------------------------------
@@ -866,6 +919,11 @@ static JNINativeMethod gMethods[] = {
 #define JAVA_POSTEVENT_CALLBACK_NAME                    "postEventFromNative"
 #define JAVA_CONST_PCM16_NAME                           "ENCODING_PCM_16BIT"
 #define JAVA_CONST_PCM8_NAME                            "ENCODING_PCM_8BIT"
+#define JAVA_CONST_AMRNB_NAME                           "ENCODING_AMRNB"
+#define JAVA_CONST_AMRWB_NAME                           "ENCODING_AMRWB"
+#define JAVA_CONST_EVRC_NAME                            "ENCODING_EVRC"
+#define JAVA_CONST_EVRCB_NAME                           "ENCODING_EVRCB"
+#define JAVA_CONST_EVRCWB_NAME                          "ENCODING_EVRCWB"
 #define JAVA_CONST_BUFFER_COUNT_NAME                    "BUFFER_COUNT"
 #define JAVA_CONST_STREAM_VOICE_CALL_NAME               "STREAM_VOICE_CALL"
 #define JAVA_CONST_STREAM_SYSTEM_NAME                   "STREAM_SYSTEM"
@@ -963,7 +1021,57 @@ int register_android_media_AudioTrack(JNIEnv *env)
                 JAVA_CONST_PCM16_NAME, &(javaAudioTrackFields.PCM16))
            || !android_media_getIntConstantFromClass(env, audioFormatClass,
                 JAVA_AUDIOFORMAT_CLASS_NAME,
-                JAVA_CONST_PCM8_NAME, &(javaAudioTrackFields.PCM8)) ) {
+                JAVA_CONST_PCM8_NAME, &(javaAudioTrackFields.PCM8))
+           || !android_media_getIntConstantFromClass(env, audioFormatClass,
+                JAVA_AUDIOFORMAT_CLASS_NAME,
+                JAVA_CONST_AMRNB_NAME, &(javaAudioTrackFields.AMRNB))
+           || !android_media_getIntConstantFromClass(env, audioFormatClass,
+                JAVA_AUDIOFORMAT_CLASS_NAME,
+                JAVA_CONST_AMRWB_NAME, &(javaAudioTrackFields.AMRWB))
+           || !android_media_getIntConstantFromClass(env, audioFormatClass,
+                JAVA_AUDIOFORMAT_CLASS_NAME,
+                JAVA_CONST_EVRC_NAME, &(javaAudioTrackFields.EVRC))
+           || !android_media_getIntConstantFromClass(env, audioFormatClass,
+                JAVA_AUDIOFORMAT_CLASS_NAME,
+                JAVA_CONST_EVRCB_NAME, &(javaAudioTrackFields.EVRCB))
+           || !android_media_getIntConstantFromClass(env, audioFormatClass,
+                JAVA_AUDIOFORMAT_CLASS_NAME,
+                JAVA_CONST_EVRCWB_NAME, &(javaAudioTrackFields.EVRCWB))
+) {
+        // error log performed in android_media_getIntConstantFromClass()
+        return -1;
+    }
+    // Get the stream types from the AudioManager class
+    jclass audioManagerClass = NULL;
+    audioManagerClass = env->FindClass(JAVA_AUDIOMANAGER_CLASS_NAME);
+    if (audioManagerClass == NULL) {
+       ALOGE("Can't find %s", JAVA_AUDIOMANAGER_CLASS_NAME);
+       return -1;
+    }
+    if ( !android_media_getIntConstantFromClass(env, audioManagerClass,
+               JAVA_AUDIOMANAGER_CLASS_NAME,
+               JAVA_CONST_STREAM_VOICE_CALL_NAME, &(javaAudioTrackFields.STREAM_VOICE_CALL))
+          || !android_media_getIntConstantFromClass(env, audioManagerClass,
+               JAVA_AUDIOMANAGER_CLASS_NAME,
+               JAVA_CONST_STREAM_MUSIC_NAME, &(javaAudioTrackFields.STREAM_MUSIC))
+          || !android_media_getIntConstantFromClass(env, audioManagerClass,
+               JAVA_AUDIOMANAGER_CLASS_NAME,
+               JAVA_CONST_STREAM_SYSTEM_NAME, &(javaAudioTrackFields.STREAM_SYSTEM))
+          || !android_media_getIntConstantFromClass(env, audioManagerClass,
+               JAVA_AUDIOMANAGER_CLASS_NAME,
+               JAVA_CONST_STREAM_RING_NAME, &(javaAudioTrackFields.STREAM_RING))
+          || !android_media_getIntConstantFromClass(env, audioManagerClass,
+               JAVA_AUDIOMANAGER_CLASS_NAME,
+               JAVA_CONST_STREAM_ALARM_NAME, &(javaAudioTrackFields.STREAM_ALARM))
+          || !android_media_getIntConstantFromClass(env, audioManagerClass,
+               JAVA_AUDIOMANAGER_CLASS_NAME,
+               JAVA_CONST_STREAM_NOTIFICATION_NAME, &(javaAudioTrackFields.STREAM_NOTIFICATION))
+          || !android_media_getIntConstantFromClass(env, audioManagerClass,
+               JAVA_AUDIOMANAGER_CLASS_NAME,
+               JAVA_CONST_STREAM_BLUETOOTH_SCO_NAME, &(javaAudioTrackFields.STREAM_BLUETOOTH_SCO))
+          || !android_media_getIntConstantFromClass(env, audioManagerClass,
+               JAVA_AUDIOMANAGER_CLASS_NAME,
+               JAVA_CONST_STREAM_DTMF_NAME, &(javaAudioTrackFields.STREAM_DTMF))) {
         // error log performed in android_media_getIntConstantFromClass()
         return -1;
     }

commit a00099d617dcf757393b14d34b52f8f398103b7c

frameworks/base/media: Fix conflicts in MediaFile type enumeration
fixes type confusion problems introduced by change: 82a6b49239fe5d9ce54e30536dfbb3ad8f9b036a

both DIVX and JPEG were assigned 31, and both EC3 and MID were assigned 16.

BIG UPS to pbeeler @ SaberMod for pointing us at the CodeAurora audio squish from CM as the culprit.

Change-Id: Ia3d1ec427af3ce02bd44631577c2f7698950c678
Signed-off-by: nuclearmistake <nuclearmistake@gmail.com>

media/java/android/media/MediaFile.java
@@ -63,24 +63,24 @@ public class MediaFile {
     private static final int LAST_AUDIO_FILE_TYPE2 = FILE_TYPE_DTS;
 
     // MIDI file types
-    public static final int FILE_TYPE_MID     = 16;
-    public static final int FILE_TYPE_SMF     = 17;
-    public static final int FILE_TYPE_IMY     = 18;
+    public static final int FILE_TYPE_MID     = 21;
+    public static final int FILE_TYPE_SMF     = 22;
+    public static final int FILE_TYPE_IMY     = 23;
     private static final int FIRST_MIDI_FILE_TYPE = FILE_TYPE_MID;
     private static final int LAST_MIDI_FILE_TYPE = FILE_TYPE_IMY;
 
     // Video file types
-    public static final int FILE_TYPE_MP4     = 21;
-    public static final int FILE_TYPE_M4V     = 22;
-    public static final int FILE_TYPE_3GPP    = 23;
-    public static final int FILE_TYPE_3GPP2   = 24;
-    public static final int FILE_TYPE_WMV     = 25;
-    public static final int FILE_TYPE_ASF     = 26;
-    public static final int FILE_TYPE_MKV     = 27;
-    public static final int FILE_TYPE_MP2TS   = 28;
-    public static final int FILE_TYPE_AVI     = 29;
-    public static final int FILE_TYPE_WEBM    = 30;
-    public static final int FILE_TYPE_DIVX    = 31;
+    public static final int FILE_TYPE_MP4     = 31;
+    public static final int FILE_TYPE_M4V     = 32;
+    public static final int FILE_TYPE_3GPP    = 33;
+    public static final int FILE_TYPE_3GPP2   = 34;
+    public static final int FILE_TYPE_WMV     = 35;
+    public static final int FILE_TYPE_ASF     = 36;
+    public static final int FILE_TYPE_MKV     = 37;
+    public static final int FILE_TYPE_MP2TS   = 38;
+    public static final int FILE_TYPE_AVI     = 39;
+    public static final int FILE_TYPE_WEBM    = 40;
+    public static final int FILE_TYPE_DIVX    = 41;
     private static final int FIRST_VIDEO_FILE_TYPE = FILE_TYPE_MP4;
     private static final int LAST_VIDEO_FILE_TYPE = FILE_TYPE_DIVX;
 
@@ -90,26 +90,26 @@ public class MediaFile {
     private static final int LAST_VIDEO_FILE_TYPE2 = FILE_TYPE_MP2PS;
 
     // Image file types
-    public static final int FILE_TYPE_JPEG    = 31;
-    public static final int FILE_TYPE_GIF     = 32;
-    public static final int FILE_TYPE_PNG     = 33;
-    public static final int FILE_TYPE_BMP     = 34;
-    public static final int FILE_TYPE_WBMP    = 35;
-    public static final int FILE_TYPE_WEBP    = 36;
+    public static final int FILE_TYPE_JPEG    = 51;
+    public static final int FILE_TYPE_GIF     = 52;
+    public static final int FILE_TYPE_PNG     = 53;
+    public static final int FILE_TYPE_BMP     = 54;
+    public static final int FILE_TYPE_WBMP    = 55;
+    public static final int FILE_TYPE_WEBP    = 56;
     private static final int FIRST_IMAGE_FILE_TYPE = FILE_TYPE_JPEG;
     private static final int LAST_IMAGE_FILE_TYPE = FILE_TYPE_WEBP;
 
     // Playlist file types
-    public static final int FILE_TYPE_M3U      = 41;
-    public static final int FILE_TYPE_PLS      = 42;
-    public static final int FILE_TYPE_WPL      = 43;
-    public static final int FILE_TYPE_HTTPLIVE = 44;
+    public static final int FILE_TYPE_M3U      = 61;
+    public static final int FILE_TYPE_PLS      = 62;
+    public static final int FILE_TYPE_WPL      = 63;
+    public static final int FILE_TYPE_HTTPLIVE = 64;
 
     private static final int FIRST_PLAYLIST_FILE_TYPE = FILE_TYPE_M3U;
     private static final int LAST_PLAYLIST_FILE_TYPE = FILE_TYPE_HTTPLIVE;
 
     // Drm file types
-    public static final int FILE_TYPE_FL      = 51;
+    public static final int FILE_TYPE_FL      = 71;
     private static final int FIRST_DRM_FILE_TYPE = FILE_TYPE_FL;
     private static final int LAST_DRM_FILE_TYPE = FILE_TYPE_FL;
 