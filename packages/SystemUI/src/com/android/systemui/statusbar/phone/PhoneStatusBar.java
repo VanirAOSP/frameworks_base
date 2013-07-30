bfd0b7ad692e03c6635c2e917668aba9ba50233f
PrimeDirective <activethrasher00@gmail.com>
2013-07-30 17:43:39 -0400

    DUMPTRUCK: add missing variable
    google lol?
    
    Change-Id: I256fcb930fc7154a874afbf939a9e3cc387db8a1

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBar.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBar.java
@@ -113,6 +113,7 @@ public class PhoneStatusBar extends BaseStatusBar {
     static final String TAG = "PhoneStatusBar";
     public static final boolean DEBUG = BaseStatusBar.DEBUG;
     public static final boolean SPEW = DEBUG;
+    public static final boolean DUMPTRUCK = true; // extra dumpsys info
     public static final boolean DEBUG_GESTURES = false;
 
     public static final boolean DEBUG_CLINGS = false;
