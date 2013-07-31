19e0ad04d96167c0c276faa17c9dd8cb098a3891
benjamingoodwin <benjamin.j.goodwin@gmail.com>
2013-07-31 10:43:01 -0300

    Dual panel for settings [1/2]
    Change-Id: I88e04bf5f789f4215bceec17b55937588ae158a1

--- core/java/android/preference/PreferenceActivity.java
+++ core/java/android/preference/PreferenceActivity.java
@@ -26,6 +26,7 @@ import android.content.Intent;
 import android.content.res.Resources;
 import android.content.res.TypedArray;
 import android.content.res.XmlResourceParser;
+import android.provider.Settings; 
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.Message;
@@ -708,9 +709,7 @@ public abstract class PreferenceActivity extends ListActivity implements
      * enough.
      */
     public boolean onIsMultiPane() {
-        boolean preferMultiPane = getResources().getBoolean(
-                com.android.internal.R.bool.preferences_prefer_dual_pane);
-        return preferMultiPane;
+        return Settings.System.getBoolean(getContentResolver(), Settings.System.FORCE_DUAL_PANEL, false); 
     }
 
     /**
