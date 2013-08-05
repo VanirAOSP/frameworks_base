bcc451b784dfaa1dab11363b7768eff37665980f
PrimeDirective <activethrasher00@gmail.com>
2013-08-05 12:50:13 -0400

    wifi: fixed to keep country code after boot
    Change-Id: I9769f557d2e8bab58ddfa89e8e2a1427d7226e7a

--- wifi/java/android/net/wifi/WifiStateMachine.java
+++ wifi/java/android/net/wifi/WifiStateMachine.java
@@ -1315,6 +1315,7 @@ public class WifiStateMachine extends StateMachine {
                 Settings.Global.WIFI_COUNTRY_CODE);
         if (countryCode != null && !countryCode.isEmpty()) {
             setCountryCode(countryCode, false);
+            mCountryCode = countryCode;
         } else {
             // On wifi-only devices, some drivers don't find hidden SSIDs unless DRIVER COUNTRY
             // is called. Use the default country code to ping the driver.
