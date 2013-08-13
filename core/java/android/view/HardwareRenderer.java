0d842b3136ad3722d329efc3d29ee50d3935ba10
nuclearmistake <nuclearmistake@gmail.com>
2013-08-13 16:53:02 -0400

    Don't getprop perpetually for something stupid nobody ever uses. thanks.
    Change-Id: I8f794b757a9e617fdc0d2528923f121750e47bf8

--- core/java/android/view/HardwareRenderer.java
+++ core/java/android/view/HardwareRenderer.java
@@ -233,8 +233,6 @@ public abstract class HardwareRenderer {
      *         false otherwise
      */
     public static boolean isAvailable() {
-        if (SystemProperties.getBoolean("ro.nohardwaregfx", false))
-            return false;
         return GLES20Canvas.isAvailable();
     }
 
