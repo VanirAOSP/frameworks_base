709e2f6b982399a7d4720fa461adcca510e2e68a
Anu6is <curtis.mayers@gmail.com>
2013-08-22 06:25:07 -0500

    Halo: Bug Fixes
     * HALO landscape orientation freeze
     * Animation clean up (remove number batch when clearing HALO)
    
    Change-Id: I0fb7862a8d0754e27e49b8ea1cab9d49f923e189
    Signed-off-by: Dan Trifan <jimsth@gmail.com>

--- packages/SystemUI/src/com/android/systemui/statusbar/halo/Halo.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/halo/Halo.java
@@ -345,9 +345,6 @@ public class Halo extends FrameLayout implements Ticker.TickerCallback {
 
         mKillX = mScreenWidth / 2;
         mKillY = mIconHalfSize;
-
-        // In the unlikely event the user still holds on to HALO just let it be
-        if (mState != State.IDLE) return;
         
         // Halo dock position
         preferences = mContext.getSharedPreferences("Halo", 0);
@@ -1349,6 +1346,7 @@ public class Halo extends FrameLayout implements Ticker.TickerCallback {
         mEffect.tickerAnimator.cancel(true);
         mEffect.mHaloNumber.setAlpha(0f);
         mEffect.mHaloNumberIcon.setAlpha(0f);
+        mEffect.mHaloNumberContainer.setAlpha(0f);
         mContentIntent = null;
         mCurrentNotficationEntry = null;
         mEffect.killTicker();
@@ -1399,7 +1397,7 @@ public class Halo extends FrameLayout implements Ticker.TickerCallback {
         if (entry.notification.getPackageName().equals("com.paranoid.halo")) {
             msgType = HaloProperties.MessageType.PINNED;
         } else if (!entry.notification.isClearable()) {
-            msgType = HaloProperties.MessageType.PINNED;
+            msgType = HaloProperties.MessageType.SYSTEM;
         } else {
             msgType = HaloProperties.MessageType.MESSAGE;
         }
