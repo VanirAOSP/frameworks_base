c68b55fc61d051315aee80d763f74a09b96145d6
drcmda <drcmda@gmail.com>
2013-08-23 13:12:15 +0100

    Halo: more bugfixes
    1. fixed size for content
    2. flip animations for tasking only
    3. batch size calculations
    4. not a bug but pinned and system both get the same icon for now
    
    Change-Id: I755cd4e88c909ab8b982e014d470791661a0041e
    Signed-off-by: Dan Trifan <jimsth@gmail.com>

--- packages/SystemUI/src/com/android/systemui/statusbar/halo/Halo.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/halo/Halo.java
@@ -1398,7 +1398,7 @@ public class Halo extends FrameLayout implements Ticker.TickerCallback {
         if (entry.notification.getPackageName().equals("com.paranoid.halo")) {
             msgType = HaloProperties.MessageType.PINNED;
         } else if (!entry.notification.isClearable()) {
-            msgType = HaloProperties.MessageType.SYSTEM;
+            msgType = HaloProperties.MessageType.PINNED;
         } else {
             msgType = HaloProperties.MessageType.MESSAGE;
         }
@@ -1441,7 +1441,7 @@ public class Halo extends FrameLayout implements Ticker.TickerCallback {
                                 if (mHideTicker) mEffect.sleep(HaloEffect.SLEEP_DELAY + HaloEffect.WAKE_TIME * 2, HaloEffect.SLEEP_TIME, false);
                             }
 
-                            tick(entry, HaloEffect.WAKE_TIME * 2, 1000, true, true, true);
+                            tick(entry, HaloEffect.WAKE_TIME * 2, 1000, true, true, false);
 
                             // Pop while not tasking, only if notification is certified fresh
                             if (mGesture != Gesture.TASK && mState != State.SILENT) mEffect.ping(mPaintHolo, HaloEffect.WAKE_TIME * 2);
