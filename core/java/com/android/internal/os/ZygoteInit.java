c21abcc487a0178cedc92479b69717f83bcecc0f
BigBrother1984 <stevewatersy@gmail.com>
2013-09-03 02:40:35 -0400

    ZygoteInit: increase GC threshold amount when preloading to postpone GC
    Patch Set 2: Hardcode it better to 1MB.
    
    Change-Id: I7a6e195b82ebe2f4bd5bbb506e19200d452fa3e5

--- core/java/com/android/internal/os/ZygoteInit.java
+++ core/java/com/android/internal/os/ZygoteInit.java
@@ -78,7 +78,7 @@ public class ZygoteInit {
     private static final int LOG_BOOT_PROGRESS_PRELOAD_END = 3030;
 
     /** when preloading, GC after allocating this many bytes */
-    private static final int PRELOAD_GC_THRESHOLD = 50000;
+    private static final int PRELOAD_GC_THRESHOLD = 1000000;
 
     private static final String ABI_LIST_ARG = "--abi-list=";
 
@@ -96,7 +96,7 @@ public class ZygoteInit {
      * The number of times that the main Zygote loop
      * should run before calling gc() again.
      */
-    static final int GC_LOOP_COUNT = 10;
+    static final int GC_LOOP_COUNT = 15;
 
     /**
      * The path of a file that contains classes to preload.
