31f091bc9684792416373102585c24966db69160
Stevespear426 <stephen.k.spear@gmail.com>
2013-09-08 23:41:05 +0200

    frameworks: Custom System Animations
    Allows setting of some system animations.
    
    ported to VanirAOSP to be used as the base for Animation Manager
    
    Change-Id: Id360abe158d36ab85c1c0d0103428d4a8867e70c

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -3612,6 +3612,28 @@ public final class Settings {
         public static final String LISTVIEW_ANIMATION = "listview_animation";
 
         /**
+         * Setting for animation controls
+         *
+         * @hide
+         */
+        public static final String[] ACTIVITY_ANIMATION_CONTROLS = new String[] {
+                "activity_open",
+                "activity_close",
+                "task_open",
+                "task_close",
+                "task_to_front",
+                "task_to_back",
+                "wallpaper_open",
+                "wallpaper_close",
+                "wallpaper_intra_open",
+                "wallpaper_intra_close",
+        };
+
+        public static final String ANIMATION_CONTROLS_DURATION = "animation_controls_duration";
+
+        public static final String ANIMATION_CONTROLS_NO_OVERRIDE = "animation_controls_no_override";
+
+        /**
          * Volume key controls ringtone or media sound stream
          *
          * @hide
