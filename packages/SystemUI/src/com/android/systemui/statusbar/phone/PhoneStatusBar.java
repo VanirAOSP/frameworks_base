84a3434f93e61851d9d99df2c689f5cb71b6de7b
nuclearmistake <nuclearmistake@gmail.com>
2013-09-10 08:15:05 -0400

    Lights Out: handle center clock and battery modes
    Handle alpha of all battieries the same as stock battery
    by adding a common container and setting its alpha.
    
    Handle center clock in the same fashion as clock.
    
    Change-Id: If4e4bc7c2d9eb4f6c773f14f1dd9a15b833e58cd
    Signed-off-by: nuclearmistake <nuclearmistake@gmail.com>

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBar.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBar.java
@@ -2023,8 +2023,7 @@ public class PhoneStatusBar extends BaseStatusBar {
             final View notifications = mStatusBarView.findViewById(R.id.notification_icon_area);
             final View systemIcons = mStatusBarView.findViewById(R.id.statusIcons);
             final View signal = mStatusBarView.findViewById(R.id.signal_cluster);
-            final View battery = mStatusBarView.findViewById(R.id.battery);
-            final View clock = mStatusBarView.findViewById(R.id.clock);
+            final View battery = mStatusBarView.findViewById(R.id.batteries_container);
 
             final AnimatorSet lightsOutAnim = new AnimatorSet();
             lightsOutAnim.playTogether(
@@ -2032,7 +2031,8 @@ public class PhoneStatusBar extends BaseStatusBar {
                     ObjectAnimator.ofFloat(systemIcons, View.ALPHA, 0),
                     ObjectAnimator.ofFloat(signal, View.ALPHA, 0),
                     ObjectAnimator.ofFloat(battery, View.ALPHA, 0.5f),
-                    ObjectAnimator.ofFloat(clock, View.ALPHA, 0.5f)
+                    ObjectAnimator.ofFloat(mClock, View.ALPHA, 0.5f),
+                    ObjectAnimator.ofFloat(mCenterClock, View.ALPHA, 0.5f)
                 );
             lightsOutAnim.setDuration(750);
 
@@ -2042,7 +2042,8 @@ public class PhoneStatusBar extends BaseStatusBar {
                     ObjectAnimator.ofFloat(systemIcons, View.ALPHA, 1),
                     ObjectAnimator.ofFloat(signal, View.ALPHA, 1),
                     ObjectAnimator.ofFloat(battery, View.ALPHA, 1),
-                    ObjectAnimator.ofFloat(clock, View.ALPHA, 1)
+                    ObjectAnimator.ofFloat(mClock, View.ALPHA, 1),
+                    ObjectAnimator.ofFloat(mCenterClock, View.ALPHA, 1)
                 );
             lightsOnAnim.setDuration(250);
 
