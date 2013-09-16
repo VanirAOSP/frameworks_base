f67bd2c91d651a5819ece8a8b48da05f9969a634
Soowon Choi <soowon.choi@lge.com>
2013-09-16 16:51:47 +0900

    Remove useless import statement
    ReentrantLock does not use in Camera class.
    
    Change-Id: I1765ebcb1bf6a0f9ac3d5da124e3d447de16faa0

--- core/java/android/hardware/Camera.java
+++ core/java/android/hardware/Camera.java
@@ -41,7 +41,6 @@ import java.lang.ref.WeakReference;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
-import java.util.concurrent.locks.ReentrantLock;
 
 /**
  * The Camera class is used to set image capture settings, start/stop preview,
