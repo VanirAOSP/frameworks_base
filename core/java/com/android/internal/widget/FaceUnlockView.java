f21c7037850a30de4f16ffb6c723e724a962ccc7
Noah Greene <chevycam94@gmail.com>
2013-10-27 23:59:36 -0400

    More unused imports
    Change-Id: I326517dd551b3d5486cafd2cf1720efba4bc9a07

--- core/java/com/android/internal/widget/FaceUnlockView.java
+++ core/java/com/android/internal/widget/FaceUnlockView.java
@@ -18,7 +18,6 @@ package com.android.internal.widget;
 
 import android.content.Context;
 import android.util.AttributeSet;
-import android.util.Log;
 import android.view.View;
 import android.widget.RelativeLayout;
 
