f1b5b65f2d40885188e8e3d6440ac67dabb79567
Ricardo Cerqueira <cyanogenmod@cerqueira.org>
2013-11-04 03:37:40 +0000

    Framework: Add Advanced reboot (2 of 2)
    This commit responds to a setting in Development settings for including
    options in the power menu for rebooting into recovery or bootloader. It
    is defauled to off.
    
    When enabled, the Advanced reboot options will only be available once the
    device is unlocked..
    
    CMhange-Id: I675f31de1f82c344c7923e953fd4a6e26dd036da
    
    Conflicts:
    	core/java/android/provider/Settings.java
    
    Conflicts:
    	core/java/android/provider/Settings.java
    	services/java/com/android/server/power/ShutdownThread.java
    
    Change-Id: Ib7f600d4387ea3f24f9c7e3b543cbebc201e75c9
    Signed-off-by: DHOMD <DHO@dho.im>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -4450,6 +4450,12 @@ public final class Settings {
         public static final String PAYMENT_SERVICE_SEARCH_URI = "payment_service_search_uri";
 
         /**
+         * Whether to include options in power menu for rebooting into recovery or bootloader
+         * @hide
+         */
+        public static final String ADVANCED_REBOOT = "advanced_reboot";
+
+        /**
          * This are the settings to be backed up.
          *
          * NOTE: Settings are backed up and restored in the order they appear
@@ -4497,7 +4503,8 @@ public final class Settings {
             MOUNT_UMS_AUTOSTART,
             MOUNT_UMS_PROMPT,
             MOUNT_UMS_NOTIFY_ENABLED,
-            UI_NIGHT_MODE
+            UI_NIGHT_MODE,
+            ADVANCED_REBOOT
         };
 
         /**
