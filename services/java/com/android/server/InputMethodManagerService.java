VanirAOSP/frameworks_base

commit 3b729ec3fd0460f248acf4e7b8f0f33eb9db69cb

Revert "Fix various broken bits from the rebase"
This reverts commit 56351c41becf548783a706a78d82c79d1b9f2792.

Conflicts:
	policy/src/com/android/internal/policy/impl/PhoneWindowManager.java

Change-Id: Ief1187bc6b0ed874032b2fa3ac12665ab864455b
Signed-off-by: DHOMD <DHO@dho.im>

services/java/com/android/server/InputMethodManagerService.java
@@ -15,8 +15,6 @@
 
 package com.android.server;
 
-import android.provider.Settings.SettingNotFoundException;
-
 import com.android.internal.app.ThemeUtils;
 import com.android.internal.content.PackageMonitor;
 import com.android.internal.inputmethod.InputMethodUtils;
@@ -407,7 +405,7 @@ public class InputMethodManagerService extends IInputMethodManager.Stub
                     Settings.System.STATUS_BAR_IME_SWITCHER),
                     false, new ContentObserver(mHandler) {
                         public void onChange(boolean selfChange) {
-                            updateFromSettingsLocked(true);
+                            updateFromSettingsLocked();
                         }
                     });
         }

services/java/com/android/server/NotificationManagerService.java
@@ -1993,8 +1993,7 @@ public class NotificationManagerService extends INotificationManager.Stub
 
                         ProfileGroup group = profileManager.getActiveProfileGroup(pkg);
                         if (group != null) {
-                            // FIXME: local variable notification is accessed from within inner class; needs to be declared final
-                            //notification = group.processNotification(notification);
+                            notification = group.processNotification(notification);
                         }
                     } catch(Throwable th) {
                         Log.e(TAG, "An error occurred profiling the notification.", th);

services/java/com/android/server/pm/PackageManagerService.java
@@ -118,7 +118,6 @@ import android.os.SystemProperties;
 import android.os.UserHandle;
 import android.os.Environment.UserEnvironment;
 import android.os.UserManager;
-import android.provider.Settings.Secure;
 import android.security.KeyStore;
 import android.security.SystemKeyStore;
 import android.text.TextUtils;
@@ -6685,7 +6684,7 @@ public class PackageManagerService extends IPackageManager.Stub {
         info.removedPackage = packageName;
         info.removedUsers = new int[] {userId};
         info.uid = UserHandle.getUid(userId, pkgSetting.appId);
-        info.sendBroadcast(false, false, false, false);
+        info.sendBroadcast(false, false, false);
     }
 
     /**

core/java/com/android/internal/util/cm/DevUtils.java
@@ -1,63 +1,0 @@
-package com.android.internal.util.cm;
-
-import android.app.ActivityManagerNative;
-import android.app.IActivityManager;
-import android.app.ActivityManager.RunningAppProcessInfo;
-import android.content.Context;
-import android.content.Intent;
-import android.content.pm.ResolveInfo;
-import android.os.Process;
-import android.os.RemoteException;
-import android.os.UserHandle;
-import android.provider.Settings;
-
-import java.util.List;
-
-public class DevUtils {
-
-    /**
-     * Kills the top most / most recent user application, but leaves out the launcher.
-     * This is function governed by {@link Settings.Secure.KILL_APP_LONGPRESS_BACK}.
-     *
-     * @param context the current context, used to retrieve the package manager.
-     * @return {@code true} when a user application was found and closed.
-     */
-    public static boolean killForegroundApplication(Context context) {
-        boolean targetKilled = false;
-        try {
-            final Intent intent = new Intent(Intent.ACTION_MAIN);
-            String defaultHomePackage = "com.android.launcher";
-            intent.addCategory(Intent.CATEGORY_HOME);
-            final ResolveInfo res = context.getPackageManager().resolveActivity(intent, 0);
-            if (res.activityInfo != null && !res.activityInfo.packageName.equals("android")) {
-                defaultHomePackage = res.activityInfo.packageName;
-            }
-            IActivityManager am = ActivityManagerNative.getDefault();
-            List<RunningAppProcessInfo> apps = am.getRunningAppProcesses();
-            for (RunningAppProcessInfo appInfo : apps) {
-                int uid = appInfo.uid;
-                // Make sure it's a foreground user application (not system,
-                // root, phone, etc.)
-                if (uid >= Process.FIRST_APPLICATION_UID && uid <= Process.LAST_APPLICATION_UID
-                        && appInfo.importance == RunningAppProcessInfo.IMPORTANCE_FOREGROUND) {
-                    if (appInfo.pkgList != null && (appInfo.pkgList.length > 0)) {
-                        for (String pkg : appInfo.pkgList) {
-                            if (!pkg.equals("com.android.systemui") && !pkg.equals(defaultHomePackage)) {
-                                am.forceStopPackage(pkg, UserHandle.USER_CURRENT);
-                                targetKilled = true;
-                                break;
-                            }
-                        }
-                    } else {
-                        Process.killProcess(appInfo.pid);
-                        targetKilled = true;
-                        break;
-                    }
-                }
-            }
-        } catch (RemoteException remoteException) {
-            // Do nothing; just let it go.
-        }
-        return targetKilled;
-    }
-}

core/res/res/values/strings.xml
@@ -1139,11 +1139,6 @@
     <string name="permdesc_movePackage">Allows the app to move app resources from internal to external media and vice versa.</string>
 
     <!-- Title of an application permission, listed so the user can choose whether they want to allow the application to do this. -->
-    <string name="permlab_preventpower">prevent power key</string>
-    <!-- Description of an application permission, listed so the user can choose whether they want to allow the application to do this. -->
-    <string name="permdesc_preventpower">Allows an application to override the power key</string>
-
-    <!-- Title of an application permission, listed so the user can choose whether they want to allow the application to do this. -->
     <string name="permlab_readLogs">read sensitive log data</string>
     <!-- Description of an application permission, listed so the user can choose whether they want to allow the application to do this. -->
     <string name="permdesc_readLogs" product="tablet">Allows the app to read from the

core/res/res/values/symbols.xml
@@ -1774,9 +1774,6 @@
   <java-symbol type="drawable" name="ic_lock_reboot" />
   <java-symbol type="drawable" name="ic_lock_profile" />
 
-  <!-- Reboot confirmation string -->
-  <java-symbol type="string" name="reboot_confirm" />
-
   <!-- Profiles -->
   <java-symbol type="xml" name="profile_default" />
   <java-symbol type="string" name="wildcardProfile" />

services/java/com/android/server/connectivity/Tethering.java
@@ -37,12 +37,10 @@ import android.net.NetworkInfo;
 import android.net.NetworkUtils;
 import android.net.RouteInfo;
 import android.os.Binder;
-import android.os.IBinder;
 import android.os.INetworkManagementService;
 import android.os.Looper;
 import android.os.Message;
 import android.os.RemoteException;
-import android.os.ServiceManager;
 import android.os.UserHandle;
 import android.provider.Settings;
 import android.util.Log;

commit 3d77565539f3686dcc1ae04f64985faa2d3b90da

fix more broken shit from 3b729ec3fd0460f248acf4e7b8f0f33eb9db69cb
please do not cherry-pick any feature related commits from other ROMs unless you are
doing a complete port of the feature.  All this is going to do is break the build and
introduce a shit ton of dead/broken code and unnecessary "reconciliations" with code we
DO NOT WANT.

Change-Id: I0ba910ee6020943524ce9eee878049da30eec584

services/java/com/android/server/InputMethodManagerService.java
@@ -405,7 +405,7 @@ public class InputMethodManagerService extends IInputMethodManager.Stub
                     Settings.System.STATUS_BAR_IME_SWITCHER),
                     false, new ContentObserver(mHandler) {
                         public void onChange(boolean selfChange) {
-                            updateFromSettingsLocked();
+                            updateFromSettingsLocked(true);
                         }
                     });
         }

services/java/com/android/server/NotificationManagerService.java
@@ -1993,7 +1993,7 @@ public class NotificationManagerService extends INotificationManager.Stub
 
                         ProfileGroup group = profileManager.getActiveProfileGroup(pkg);
                         if (group != null) {
-                            notification = group.processNotification(notification);
+                        //    notification = group.processNotification(notification);
                         }
                     } catch(Throwable th) {
                         Log.e(TAG, "An error occurred profiling the notification.", th);

services/java/com/android/server/pm/PackageManagerService.java
@@ -117,6 +117,7 @@ import android.os.SystemProperties;
 import android.os.UserHandle;
 import android.os.Environment.UserEnvironment;
 import android.os.UserManager;
+import android.provider.Settings.Secure;
 import android.security.KeyStore;
 import android.security.SystemKeyStore;
 import android.text.TextUtils;
@@ -6656,7 +6657,7 @@ public class PackageManagerService extends IPackageManager.Stub {
         info.removedPackage = packageName;
         info.removedUsers = new int[] {userId};
         info.uid = UserHandle.getUid(userId, pkgSetting.appId);
-        info.sendBroadcast(false, false, false);
+        info.sendBroadcast(false, false, false, false);
     }
 
     /**