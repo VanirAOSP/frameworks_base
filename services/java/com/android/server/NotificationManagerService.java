0fa2d4523ce99e3af0ca22ce5350093abecc06b3
DHOMD <DHO@dho.im>
2013-11-11 19:22:50 -0500

    Revert "Fix up notification LED behaviour"
    This reverts commit f5ce5a83149dfbcc05b213769f480b558b5c5159.
    
    Change-Id: I40a9ce0e463245f60a155d4ad3f802da13591dd3
    Signed-off-by: DHOMD <DHO@dho.im>

--- services/java/com/android/server/NotificationManagerService.java
+++ services/java/com/android/server/NotificationManagerService.java
@@ -1260,7 +1260,6 @@ public class NotificationManagerService extends INotificationManager.Stub
                 if (mScreenOn) {
                     mNotificationLight.turnOff();
                 }
-                updateNotificationPulse();
             } else if (action.equals(TelephonyManager.ACTION_PHONE_STATE_CHANGED)) {
                 mInCall = (intent.getStringExtra(TelephonyManager.EXTRA_STATE).equals(
                         TelephonyManager.EXTRA_STATE_OFFHOOK));
@@ -2119,8 +2118,6 @@ public class NotificationManagerService extends INotificationManager.Stub
                     if ((notification.flags & Notification.FLAG_SHOW_LIGHTS) != 0
                             && canInterrupt) {
                         mLights.add(r);
-                        // force reevaluation of active light
-                        mLedNotification = null;
                         updateLightsLocked();
                     } else {
                         if (old != null
@@ -2413,17 +2410,21 @@ public class NotificationManagerService extends INotificationManager.Stub
     {
         // handle notification lights
         if (mLedNotification == null) {
-            // use most recent light with highest score
-            for (int i = mLights.size(); i > 0; i--) {
-                NotificationRecord r = mLights.get(i - 1);
-                if (mLedNotification == null || r.score > mLedNotification.score) {
-                    mLedNotification = r;
-                }
+            // get next notification, if any
+            int n = mLights.size();
+            if (n > 0) {
+                mLedNotification = mLights.get(n-1);
             }
         }
 
+        if (mLedNotification == null) {
+            mNotificationLight.turnOff();
+            return;
+        }
+
         // Don't flash while we are in a call, screen is on or we are in quiet hours with light dimmed
-        if (mLedNotification == null || mInCall || mScreenOn || (inQuietHours() && mQuietHoursDim)) {
+        if (mLedNotification == null || mInCall
+                || (mScreenOn && !mDreaming) || (inQuietHours() && mQuietHoursDim)) {
             mNotificationLight.turnOff();
         } else {
             final Notification ledno = mLedNotification.sbn.getNotification();
@@ -2594,7 +2595,6 @@ public class NotificationManagerService extends INotificationManager.Stub
 
             pw.println("  mSoundNotification=" + mSoundNotification);
             pw.println("  mVibrateNotification=" + mVibrateNotification);
-            pw.println("  mLedNotification=" + mLedNotification);
             pw.println("  mDisabledNotifications=0x" + Integer.toHexString(mDisabledNotifications));
             pw.println("  mSystemReady=" + mSystemReady);
             pw.println("  mArchive=" + mArchive.toString());
