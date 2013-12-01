af7e8f1a723cbcdc24016e46fd9cdf19ddd4591c
maxwen <max.weninger@gmail.com>
2013-12-01 16:47:13 +0100

    [1/3] base: allow disabling non intrusive incall ui
    Change-Id: Ibe9c65f1fe024a859d98d96fd86199607a1060de

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -3495,6 +3495,13 @@ public final class Settings {
         public static final String SYSTEMUI_RECENTS_MEM_DISPLAY = "vanir_interface_recents_mem_display";        
 
         /**
+         * Should the non-intrsive incall ui be used
+         *
+         * @hide
+         */
+        public static final String NON_INTRUSIVE_INCALL = "non_intrusive_incall";
+
+        /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
          *
