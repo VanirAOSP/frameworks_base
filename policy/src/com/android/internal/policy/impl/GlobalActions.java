c551d3c7a5c8357ace6042345f5276f66565ffa5
Christian Morlok <ChristianMorlok@gmail.com>
2013-12-09 19:34:06 +0100

    [1/2] Base: Hide power menu on lockscreen
    Adds an option to enable/disable power menu on lockscreen.
    
    Settings: https://gerrit.omnirom.org/#/c/4702/
    
    Change-Id: Ie6e0f428a00c6e7eb7b3fd47bd9b4f85e0e2131e
    Conflicts:
    	core/java/android/provider/Settings.java
    Conflicts:
    	core/java/android/provider/Settings.java
    	policy/src/com/android/internal/policy/impl/PhoneWindowManager.java

--- policy/src/com/android/internal/policy/impl/GlobalActions.java
+++ policy/src/com/android/internal/policy/impl/GlobalActions.java
@@ -141,6 +141,7 @@ class GlobalActions implements DialogInterface.OnDismissListener, DialogInterfac
     // power reboot dialog
     private boolean mStockMode = false;
     private boolean showBugReport;
+    private boolean AllowPowerMenuOnLockscreen;
     private boolean showScreenshot;
     private boolean showGlobalImmersiveMode;
     private boolean powerMenuImmersiveMode;
@@ -209,18 +210,20 @@ class GlobalActions implements DialogInterface.OnDismissListener, DialogInterfac
 
         mKeyguardLocked = keyguardLocked;
         mDeviceProvisioned = isDeviceProvisioned;
-        if (mDialog != null) {
-            if (mUiContext != null) {
-                mUiContext = null;
-            }
-            mDialog.dismiss();
-            mDialog = null;
-            if (mQuickCam && !isKeyguardEnabled()) {
-                launchCameraAssistant();
-            }
-        } else {
-            mDialog = createDialog();
-            handleShow();
+        if (!keyguardLocked || (keyguardLocked && AllowPowerMenuOnLockscreen)) {
+           if (mDialog != null) {
+               if (mUiContext != null) {
+                   mUiContext = null;
+               }
+               mDialog.dismiss();
+               mDialog = null;
+               if (mQuickCam && !isKeyguardEnabled()) {
+                   launchCameraAssistant();
+               }
+           } else {
+               mDialog = createDialog();
+               handleShow();
+           }
         }
     }
 
@@ -1432,6 +1435,10 @@ class GlobalActions implements DialogInterface.OnDismissListener, DialogInterfac
                 UserHandle.USER_CURRENT) == 1;
         expanded = Settings.System.getIntForUser(cr, Settings.System.EXPANDED_DESKTOP, 0,
                 UserHandle.USER_CURRENT) == 1;
+
+        AllowPowerMenuOnLockscreen =
+                Settings.System.getIntForUser(cr,
+                        Settings.System.LOCKSCREEN_ENABLE_POWER_MENU, 1, UserHandle.USER_CURRENT) == 1;
 
         if (!mStockMode) {
             showScreenshot = Settings.System.getIntForUser(cr,
