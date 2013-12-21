d056aea859a6517542c155c8b177eb5ef7f185b8
nuclearmistake <nuclearmistake@gmail.com>
2013-12-21 12:59:57 -0500

    Update brightness slider code from aokp
    and fix a race condition in the event handling
    
    Squashed commit of the following:
    
    commit b2fb70e86a410c49abe9d058fe61fd3dbafd7ca8
    Author: Roman Birg <romanbirg@gmail.com>
    Date:   Thu Nov 22 14:03:58 2012 -0600
    
        SystemUI: re-implement statusbar brightness slider
    
        Change-Id: I96db3d8f40adf3726f4140c3ad3a7122e0ad6add
        Signed-off-by: Roman Birg <romanbirg@gmail.com>
    
    commit 3ce32b8ab061406ded11090adfb7c6f780a718ca
    Author: nuclearmistake <nuclearmistake@gmail.com>
    Date:   Sat Dec 21 12:20:49 2013 -0500
    
        Revert "systemui: add brightness slider 2/2"
    
        This reverts commit 34233ec666fc91c15e9755d493eb6cf1e49b0ced.
    
    Change-Id: Id12a55f2a93cec9553509b9ff6c35d898264cec4

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -3565,6 +3565,13 @@ public final class Settings {
          */
         public static final String SYSTEM_POWER_CRT_MODE = "system_power_crt_mode";
 
+       /**
+         * Whether to enable swiping your finger across the statusbar to change the brightness.
+         * Boolean value. Defaults to true.
+         * @hide
+         */
+        public static final String STATUSBAR_BRIGHTNESS_SLIDER = "statusbar_brightness_slider";
+
         /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
@@ -3891,15 +3898,6 @@ public final class Settings {
         @Deprecated
         public static final String WIFI_WATCHDOG_PING_TIMEOUT_MS =
             Secure.WIFI_WATCHDOG_PING_TIMEOUT_MS;
-
-        /**
-         * Boolean value whether to allow the statusbar to be used as a
-         * brightness slider when collapsed
-         *
-         * @hide
-         */
-        public static final String STATUSBAR_BRIGHTNESS_SLIDER = "statusbar_brightness_slider";
-
     }
 
     /**
