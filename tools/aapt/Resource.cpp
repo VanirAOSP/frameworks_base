685b9c528ab39dbe330f22d4c11a206ad0a08e8b
JBirdVegas <jbirdvegas@gmail.com>
2014-01-12 12:54:44 +0100

    aapt: suppress string resources build warnings
    warnings suppressed are:
    aapt: warning: **** string '%s' has no default or required localization for
    aapt: warning: string '%s' has no default translation in
    
    Patch Set 2: move ifdef and only suppress warnings for packages that
                 define NO_DEFAULT_TRANSLATION_WARNINGS and/or
                 NO_LOCALIZATION_WARNING
    
    Patch Set 3: by default do not show warnings unless
                 SHOW_DEFAULT_TRANSLATION_WARNINGS and/or
                 SHOW_LOCALIZATION_WARNINGS
    
    Change-Id: I50ef2a623959a1cc53053b446328df8e34cc99e8
    
    aapt: silence these warnings
    
    to show them, set SHOW_MANIFEST_WARNING
    
    Change-Id: I8fd3617e03ca535df1ae0ebdb3dde3a67eb2737b
    
    aapt: i hate if (true) statements
    
    Change-Id: If0d39f4d942067ae2d4ee4d32490b6e079a5f60d
    
    aapt: we dont care about uncommented public symbols
    
    export SHOW_UNCOMMENTED_SYMBOL_WARNING to show them
    
    Change-Id: I6e6461b54e5cd3a9c09efeb8df3172e48bbfa8e5
    
    aapt: introduce SHOW_EXTENDED_WARNINGS
    
    a quick switch for enabling various warnings we silenced
    
    export SHOW_EXTENDED_WARNINGS to show all silenced warnings
    
    Change-Id: I298f36469d78e335a461f5ccfc2a66aa2976ceac

--- tools/aapt/Resource.cpp
+++ tools/aapt/Resource.cpp
@@ -38,6 +38,13 @@ static const bool kIsDebug = false;
 // Number of threads to use for preprocessing images.
 static const size_t MAX_THREADS = 4;
 
+#ifdef SHOW_EXTENDED_WARNINGS
+#define SHOW_MANIFEST_WARNING
+#define SHOW_UNCOMMENTED_SYMBOL_WARNING
+#define SHOW_LOCALIZATION_WARNINGS
+#define SHOW_DEFAULT_TRANSLATION_WARNINGS
+#endif
+
 // ==========================================================================
 // ==========================================================================
 // ==========================================================================
@@ -680,7 +687,7 @@ static bool applyFileOverlay(Bundle *bundle,
                             baseGroup->removeFile(baseFileIndex);
                         } else {
                             // didn't find a match fall through and add it..
-                            if (true || bundle->getVerbose()) {
+                            if (bundle->getVerbose()) {
                                 printf("nothing matches overlay file %s, for flavor %s\n",
                                         overlayGroup->getLeaf().string(),
                                         overlayFiles.keyAt(overlayGroupIndex).toString().string());
@@ -754,9 +761,11 @@ bool addTagAttribute(const sp<XMLNode>& node, const char* ns8,
             return false;
         }
 
+#ifdef SHOW_MANIFEST_WARNING
         fprintf(stderr, "Warning: AndroidManifest.xml already defines %s (in %s);"
                         " using existing value in manifest.\n",
                 String8(attr).string(), String8(ns).string());
+#endif
 
         // don't stop the build.
         return true;
@@ -2544,9 +2553,11 @@ static status_t writeSymbolClass(
                     "%s/** %s\n",
                     getIndentSpace(indent), cmt.string());
         } else if (sym.isPublic && !includePrivate && kIsDebug) {
+#ifdef SHOW_UNCOMMENTED_SYMBOL_WARNING
             sym.sourcePos.warning("No comment for public symbol %s:%s/%s",
                 assets->getPackage().string(), className.string(),
                 String8(sym.name).string());
+#endif
         }
         String16 typeComment(sym.typeComment);
         if (typeComment.size() > 0) {
@@ -2590,9 +2601,11 @@ static status_t writeSymbolClass(
                     getIndentSpace(indent), cmt.string(),
                     getIndentSpace(indent));
         } else if (sym.isPublic && !includePrivate && kIsDebug) {
+#ifdef SHOW_UNCOMMENTED_SYMBOL_WARNING
             sym.sourcePos.warning("No comment for public symbol %s:%s/%s",
                 assets->getPackage().string(), className.string(),
                 String8(sym.name).string());
+#endif
         }
         ann.printAnnotations(fp, getIndentSpace(indent));
         fprintf(fp, "%spublic static final String %s=\"%s\";\n",
