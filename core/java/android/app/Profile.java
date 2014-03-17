8f8957c176eafe274c710f1373c9368cdcd32137
PrimeDirective <activethrasher00@gmail.com>
2014-03-17 10:50:29 -0400

    profiles: refactor set expanded desktop in profiles for immersive desktop [1/2]
    expanded desktop is outdated so the following change was reverted.  this commit brings it back
    to be compatible with our improvements.
    https://github.com/VanirAOSP/frameworks_base/commit/0501cc6e9b95a47eac0428fe5ad0a8a2cb8fcc43
    
    Change-Id: I92f6f97c4d9487d5a4585b65c53ce251a50dfe2e

--- core/java/android/app/Profile.java
+++ core/java/android/app/Profile.java
@@ -21,6 +21,8 @@ import android.media.AudioManager;
 import android.os.Parcel;
 import android.os.ParcelUuid;
 import android.os.Parcelable;
+import android.os.UserHandle;
+import android.provider.Settings;
 import android.text.TextUtils;
 import android.util.Log;
 
@@ -77,10 +79,19 @@ public final class Profile implements Parcelable, Comparable {
 
     private int mScreenLockMode = LockMode.DEFAULT;
 
+    private int mImmersiveDesktopMode = ImmersiveDesktopMode.DEFAULT;
+
     /** @hide */
     public static class LockMode {
         public static final int DEFAULT = 0;
         public static final int INSECURE = 1;
+        public static final int DISABLE = 2;
+    }
+
+    /** @hide */
+    public static class ImmersiveDesktopMode {
+        public static final int DEFAULT = 0;
+        public static final int ENABLE = 1;
         public static final int DISABLE = 2;
     }
 
@@ -346,6 +357,7 @@ public final class Profile implements Parcelable, Comparable {
         dest.writeParcelable(mAirplaneMode, flags);
         dest.writeInt(mScreenLockMode);
         dest.writeMap(mTriggers);
+        dest.writeInt(mImmersiveDesktopMode);
     }
 
     /** @hide */
@@ -379,6 +391,7 @@ public final class Profile implements Parcelable, Comparable {
         mAirplaneMode = (AirplaneModeSettings) in.readParcelable(null);
         mScreenLockMode = in.readInt();
         in.readMap(mTriggers, null);
+        mImmersiveDesktopMode = in.readInt();
     }
 
     public String getName() {
@@ -466,6 +479,20 @@ public final class Profile implements Parcelable, Comparable {
         mDirty = true;
     }
 
+    public int getImmersiveDesktopMode() {
+        return mImmersiveDesktopMode;
+    }
+
+    public void setImmersiveDesktopMode(int immersiveDesktopMode) {
+        if (immersiveDesktopMode < ImmersiveDesktopMode.DEFAULT
+                || immersiveDesktopMode > ImmersiveDesktopMode.DISABLE) {
+            mImmersiveDesktopMode = ImmersiveDesktopMode.DEFAULT;
+        } else {
+            mImmersiveDesktopMode = immersiveDesktopMode;
+        }
+        mDirty = true;
+    }
+
     public AirplaneModeSettings getAirplaneMode() {
         return mAirplaneMode;
     }
@@ -537,6 +564,10 @@ public final class Profile implements Parcelable, Comparable {
         builder.append("<screen-lock-mode>");
         builder.append(mScreenLockMode);
         builder.append("</screen-lock-mode>\n");
+
+        builder.append("<immersive-desktop-mode>");
+        builder.append(mImmersiveDesktopMode);
+        builder.append("</immersive-desktop-mode>\n");
 
         mAirplaneMode.getXmlString(builder, context);
 
@@ -669,6 +700,9 @@ public final class Profile implements Parcelable, Comparable {
                 if (name.equals("screen-lock-mode")) {
                     profile.setScreenLockMode(Integer.valueOf(xpp.nextText()));
                 }
+                if (name.equals("immersive-desktop-mode")) {
+                    profile.setImmersiveDesktopMode(Integer.valueOf(xpp.nextText()));
+                }
                 if (name.equals("profileGroup")) {
                     ProfileGroup pg = ProfileGroup.fromXml(xpp, context);
                     profile.addProfileGroup(pg);
@@ -713,7 +747,16 @@ public final class Profile implements Parcelable, Comparable {
         mRingMode.processOverride(context);
         // Set airplane mode
         mAirplaneMode.processOverride(context);
+
+        // Set immersive desktop
+        if (mImmersiveDesktopMode != ImmersiveDesktopMode.DEFAULT) {
+            Settings.System.putIntForUser(context.getContentResolver(),
+                    Settings.System.GLOBAL_IMMERSIVE_MODE_STATE,
+                    mImmersiveDesktopMode == ImmersiveDesktopMode.ENABLE ? 1 : 0,
+                    UserHandle.USER_CURRENT);
+        }
     }
+
 
     /** @hide */
     public StreamSettings getSettingsForStream(int streamId){
