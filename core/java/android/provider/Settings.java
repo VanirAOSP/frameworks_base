57f8e53f8c831d7e6557f8c589ea9be802967086
Alexander Martinz <eviscerationls@gmail.com>
2014-05-03 10:02:10 -0300

    [1/2] base: Add Haptic Feedback to quicktiles
    PS2: Start fresh
    PS3: Redo like fliptiles
    PS4: Proper method scope. tiles can override isVibrationEnabled()
         where it makes sense.
         Clean up merge fail. check for vibrator support before
         we query settings provider possible cutting back on
         unneeded queries. [From BRD @ EOS Review]
    PS5: Changed class (or whatever I'm so new to this) and remove
         obslete code.
    
    Change-Id: Ia3bbb6ef90a9f7b3cf45c341d3b95eb131a96694

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -3001,12 +3001,19 @@ public final class Settings {
          */
         public static final String EGG_MODE = "egg_mode";
 
-       /**
+        /**
          * Animate-flip Quick Settings Panel Tiles on click
          *
          * @hide
          */
         public static final String QUICK_SETTINGS_TILES_FLIP = "quick_settings_tiles_flip";
+
+        /**
+         * Let Quick Settings Panel Tiles vibrate on click
+         *
+         * @hide
+         */
+        public static final String QUICK_SETTINGS_TILES_VIBRATE = "quick_settings_tiles_vibrate";
 
         /**
          * Quick Settings Panel Tiles to Use
