094a7781f4b3da9aec8cd240e0fca05f16956ede
DHOMD <DHO@dho.im>
2014-05-17 11:04:19 -0400

    Squashed revert of temp removal of CM 11 engine
    This is a combination of 6 commits.
    The first commit's message is:
    
    Revert "TEMP - Remove new and borked theme engine #3"
    
    This reverts commit 4b5d87224b3102845a6131233d7d00c0fd7cf240.
    
    Conflicts:
    	policy/src/com/android/internal/policy/impl/PhoneWindowManager.java
    
    hange-Id: Ida65ea05b99c37ee04d4e83a04c27792d43cd463
    Signed-off-by: DHOMD <DHO@dho.im>
    
    Revert "Revert "Return system theme when SecurityException is caught.""
    
    This reverts commit a71056be776e87c87b17dcbde9b3ab481ef8d016.
    
    Revert "Revert "CM11 Themes: Ensure customTheme is not null.""
    
    This reverts commit 6d3b23775cceddd38e334858efdf8c32183ca6c2.
    
    Revert "Revert "CM11 Themes: Properly handle SystemUI theme package name.""
    
    This reverts commit df46bef93d33a45924c4b34ac10c5efabb6482c8.
    
    Revert "Revert "CM11 Themes: Include alarms in mixnmatch.""
    
    This reverts commit fef4d1352617c4ec58771629e3a4dcdf1b9c94be.
    
    Revert "Revert "CM11 Themes: Common overlay for sharing resources. [1/2]""
    
    This reverts commit 640b88e4dee031c7efe0e76f57c52010cc0b498f.
    
    Change-Id: I86e6837ed73442e201f7a4ce7805432c28934ef7
    Signed-off-by: DHOMD <DHO@dho.im>
    Conflicts:
    	core/jni/AndroidRuntime.cpp
    Signed-off-by: DHOMD <DHO@dho.im>

--- policy/src/com/android/internal/policy/impl/PhoneWindowManager.java
+++ policy/src/com/android/internal/policy/impl/PhoneWindowManager.java
@@ -69,8 +69,6 @@ import android.os.UserHandle;
 import android.os.Vibrator;
 import android.provider.Settings;
 
-import com.android.internal.app.ThemeUtils;
-
 import android.service.dreams.DreamService;
 import android.service.dreams.IDreamManager;
 import android.service.gesture.EdgeGestureManager;
@@ -263,7 +261,6 @@ public class PhoneWindowManager implements WindowManagerPolicy {
     private final Object mQuickBootLock = new Object();
 
     Context mContext;
-    Context mUiContext;
     IWindowManager mWindowManager;
     WindowManagerFuncs mWindowManagerFuncs;
     PowerManager mPowerManager;
@@ -1137,7 +1134,7 @@ public class PhoneWindowManager implements WindowManagerPolicy {
             // Do the switch
             final AudioManager am = (AudioManager)mContext.getSystemService(Context.AUDIO_SERVICE);
             final int ringerMode = am.getRingerMode();
-            final VolumePanel volumePanel = new VolumePanel(ThemeUtils.createUiContext(mContext),
+            final VolumePanel volumePanel = new VolumePanel(mContext,
                                                               (AudioService) getAudioService());
             if (ringerMode == AudioManager.RINGER_MODE_NORMAL) {
                 boolean vibrateSetting = Settings.System.getIntForUser(mContext.getContentResolver(),
@@ -2351,14 +2348,13 @@ public class PhoneWindowManager implements WindowManagerPolicy {
             if (DEBUG_STARTING_WINDOW) Slog.d(TAG, "addStartingWindow " + packageName
                     + ": nonLocalizedLabel=" + nonLocalizedLabel + " theme="
                     + Integer.toHexString(theme));
-
-            try {
-                context = context.createPackageContext(packageName, 0);
-                if (theme != context.getThemeResId()) {
+            if (theme != context.getThemeResId() || labelRes != 0) {
+                try {
+                    context = context.createPackageContext(packageName, 0);
                     context.setTheme(theme);
+                } catch (PackageManager.NameNotFoundException e) {
+                    // Ignore
                 }
-            } catch (PackageManager.NameNotFoundException e) {
-                // Ignore
             }
 
             // Construct the Toast
@@ -5466,12 +5462,6 @@ public class PhoneWindowManager implements WindowManagerPolicy {
                     updateSystemUiVisibilityLw();
                 }
             }
-        }
-    };
-
-    BroadcastReceiver mThemeChangeReceiver = new BroadcastReceiver() {
-        public void onReceive(Context context, Intent intent) {
-            mUiContext = null;
         }
     };
 
