cd234b93de416caf5ac590814c786d4ed0d9e99d
Andy Mast <andy@cyngn.com>
2014-06-04 15:40:19 -0700

    Don't cleanup overlay mapping for non-theme apps
    mOverlay structure should keep all app/theme mappings even
    if the app does not exist. Otherwise we will have to insert back the mapping
    for every app that is installed which would be time consuming.
    
    Without this patch an upgrade app loses its applied theme
    until the device is rebooted.
    
    Change-Id: Id02b8e0a3bdd78d1a9d39cd38294c584ef95f8db

--- services/java/com/android/server/pm/PackageManagerService.java
+++ services/java/com/android/server/pm/PackageManagerService.java
@@ -5629,7 +5629,6 @@ public class PackageManagerService extends IPackageManager.Stub {
            String idmapPath = getIdmapPath(appPkg, opkg);
            new File(idmapPath).delete();
         }
-        mOverlays.remove(appPkg.packageName);
     }
 
     private void recursiveDelete(File f) {
