457916e3fd9619ccbbd51bbdfd54c2699e65d84f
PrimeDirective <activethrasher00@gmail.com>
2014-07-08 15:59:27 -0400

    Navbar: fix uninitialized value for allow legacy menu keys
    This setting will now restore after rebooting.
    
    Change-Id: I1068787a5b35403a9dfec3447aa270dc5a5def57

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/NavigationBarView.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/NavigationBarView.java
@@ -250,6 +250,7 @@ public class NavigationBarView extends LinearLayout {
         mBarTransitions = new NavigationBarTransitions(this);
 
         mUserButtons = Settings.System.getString(mContext.getContentResolver(), Settings.System.NAVIGATION_BAR_BUTTONS);
+        mSideKeys = Settings.System.getInt(r, Settings.System.NAVIGATION_BAR_SIDEKEYS, 1) == 1;
 
         mCameraDisabledByDpm = isCameraDisabledByDpm();
         watchForDevicePolicyChanges();
