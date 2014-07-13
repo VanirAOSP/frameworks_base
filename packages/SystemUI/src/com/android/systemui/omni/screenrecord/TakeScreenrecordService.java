392e644084217ee239f18ff9f025807a610f500d
XpLoDWilD <xplod@ouverta.fr>
2014-07-13 21:43:28 +0200

    screenrecord: Fix SystemUI loop-crash
    Change-Id: I25045820465d21b7d1f0e933e5e4f3324cb044b6
    Signed-off-by: Dan Trifan <jimsth@gmail.com>

--- packages/SystemUI/src/com/android/systemui/omni/screenrecord/TakeScreenrecordService.java
+++ packages/SystemUI/src/com/android/systemui/omni/screenrecord/TakeScreenrecordService.java
@@ -72,16 +72,18 @@ public class TakeScreenrecordService extends Service {
 
     @Override
     public int onStartCommand(Intent intent, int flags, int startId) {
-        if (intent.getAction().equals(ACTION_START)) {
-            startScreenrecord();
-        } else if (intent.getAction().equals(ACTION_STOP)) {
-            stopScreenrecord();
-        } else if (intent.getAction().equals(ACTION_TOGGLE_POINTER)) {
-            int currentStatus = Settings.System.getInt(getContentResolver(),
-                        Settings.System.SHOW_TOUCHES, 0);
-            Settings.System.putInt(getContentResolver(), Settings.System.SHOW_TOUCHES,
-                        1 - currentStatus);
-            mScreenrecord.updateNotification();
+        if (intent != null) {
+            if (intent.getAction().equals(ACTION_START)) {
+                startScreenrecord();
+            } else if (intent.getAction().equals(ACTION_STOP)) {
+                stopScreenrecord();
+            } else if (intent.getAction().equals(ACTION_TOGGLE_POINTER)) {
+                int currentStatus = Settings.System.getInt(getContentResolver(),
+                            Settings.System.SHOW_TOUCHES, 0);
+                Settings.System.putInt(getContentResolver(), Settings.System.SHOW_TOUCHES,
+                            1 - currentStatus);
+                mScreenrecord.updateNotification();
+            }
         }
 
         return super.onStartCommand(intent, flags, startId);
