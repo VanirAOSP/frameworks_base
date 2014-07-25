001b908ef9495b199aaf418623aa1353ca416781
DHOMD <DHO@dho.im>
2014-07-25 19:30:33 -0400

    Revert "Add system setting for t9 search input locale"
    This reverts commit a95e6813abce619ac85a7798f45d499f65724b40.

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -4631,12 +4631,6 @@ public final class Settings {
         public static final String RECENT_PANEL_EXPANDED_MODE = "recent_panel_expanded_mode";
 
         /**
-         * Locale for secondary overlay on dialer for t9 search input
-         * @hide
-         */
-        public static final String T9_SEARCH_INPUT_LOCALE = "t9_search_input_locale";
-
-        /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
          *
