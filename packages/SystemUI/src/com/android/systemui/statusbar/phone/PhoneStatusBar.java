8a78e4978a7fb5945256ddfe361a9ed5fc57a475
nuclearmistake <nuclearmistake@gmail.com>
2014-08-02 12:34:16 -0400

    vanir adaptations of navbar bg themeing
    Change-Id: Ib22ffd3abdaf7b52ad15061d86130aad70c5eeb8

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBar.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBar.java
@@ -478,6 +478,7 @@ public class PhoneStatusBar extends BaseStatusBar implements DemoMode,
                 mNavigationBarView = (NavigationBarView) View.inflate(mContext, R.layout.navigation_bar, null);
             }
             mNavigationBarView.setDisabledFlags(mDisabled);
+            mNavigationBarView.updateResources(getNavbarThemedResources());
             mNavigationBarView.setBar(this);
             mNavigationBarView.setOnTouchListener(new View.OnTouchListener() {
                 @Override
@@ -1296,6 +1297,18 @@ public class PhoneStatusBar extends BaseStatusBar implements DemoMode,
         lp.setTitle("NavigationBar");
         lp.windowAnimations = 0;
         return lp;
+    }
+
+    private Resources getNavbarThemedResources() {
+        String pkgName = mCurrentTheme.getOverlayPkgNameForApp(ThemeConfig.SYSTEMUI_NAVBAR_PKG);
+        Resources res = null;
+        try {
+            res = mContext.getPackageManager().getThemedResourcesForApplication(
+                    mContext.getPackageName(), pkgName);
+        } catch (PackageManager.NameNotFoundException e) {
+            res = mContext.getResources();
+        }
+        return res;
     }
 
     private void addHeadsUpView() {
