96a8a7d64e60b2e266cf6b752cf05e0984b1181b
PrimeDirective <activethrasher00@gmail.com>
2014-08-06 19:54:16 -0400

    Navbar: alternate layouts
    allow up to 5 customizable alternate layouts and add special arrow keys
    on each side to switch layouts.  Also introduces a number of optimizations
    for setting up the navbar.
    
    to do list:
    perhaps use a single string with delimiters per string.. and allow an indefinite
    number of layout additions
    
    Change-Id: I2bfddb5967a77c63e1d403d1defa01bb3d123d42

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBar.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBar.java
@@ -3654,6 +3654,11 @@ public class PhoneStatusBar extends BaseStatusBar implements DemoMode,
     }
 
     @Override
+    public void notifyLayoutChange(int direction) {
+        mNavigationBarView.notifyLayoutChange(direction);
+    }
+
+    @Override
     public void setButtonDrawable(int buttonId, int iconId) {
         mNavigationBarView.setButtonDrawable(buttonId, iconId);
     }
