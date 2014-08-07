07bf2c3fd3f75c8ef5aff1d25c2a8735ddf13ea6
Dan Trifan <jimsth@gmail.com>
2014-08-07 08:49:02 +0300

    quiet hours: refactor system sounds
    thanks to SlimRom
    
    Change-Id: Ice90016f88cb3b5a35d860afec574d272d747d03
    Signed-off-by: Dan Trifan <jimsth@gmail.com>

--- packages/SystemUI/src/com/android/systemui/screenshot/GlobalScreenshot.java
+++ packages/SystemUI/src/com/android/systemui/screenshot/GlobalScreenshot.java
@@ -44,6 +44,7 @@ import android.os.AsyncTask;
 import android.os.Environment;
 import android.os.Process;
 import android.provider.MediaStore;
+import android.provider.Settings;
 import android.util.DisplayMetrics;
 import android.view.Display;
 import android.view.LayoutInflater;
@@ -539,7 +540,10 @@ class GlobalScreenshot {
             @Override
             public void run() {
                 // Play the shutter sound to notify that we've taken a screenshot
-                mCameraSound.play(MediaActionSound.SHUTTER_CLICK);
+                if (Settings.System.getInt(mContext.getContentResolver(),
+                        Settings.System.QUIET_HOURS_SYSTEM, 1) != 2) {
+                    mCameraSound.play(MediaActionSound.SHUTTER_CLICK);
+                }
 
                 mScreenshotView.setLayerType(View.LAYER_TYPE_HARDWARE, null);
                 mScreenshotView.buildLayer();
