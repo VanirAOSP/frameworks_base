2d60a6c91747c7f84df3b65296893293924801f8
PrimeDirective <activethrasher00@gmail.com>
2014-08-19 01:24:27 -0400

    fix necessity for try/catch on reorient() during adding NBV
    Change-Id: I0d0fa7129156750d47b406919200c075d19cc54a

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/NavigationBarTransitions.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/NavigationBarTransitions.java
@@ -85,10 +85,6 @@ public final class NavigationBarTransitions extends BarTransitions {
         View[] views = mView.getAllButtons();
 
         for(View v : views) {
-            if (AwesomeConstant.ACTION_BACK.value().equals(v.getTag())) {
-                // back button was skipped in original calculations
-                continue;
-            }
             setKeyButtonViewQuiescentAlpha(v, alpha, animate);
         }
 
@@ -110,10 +106,6 @@ public final class NavigationBarTransitions extends BarTransitions {
         float backAlpha = 0;
         View[] views = mView.getAllButtons();
         for(View v : views) {
-            if (AwesomeConstant.ACTION_BACK.value().equals(v.getTag())) {
-                // back button was skipped in original calculations
-                continue;
-            }
             backAlpha = maxVisibleQuiescentAlpha(backAlpha, v);
         }
 
