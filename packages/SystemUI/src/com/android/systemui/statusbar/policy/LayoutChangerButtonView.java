8830c6e2f5a48322b6db44102263b94419fc1fbc
PrimeDirective <activethrasher00@gmail.com>
2014-09-16 00:26:09 -0400

    Navbar: add extensibility of KBV for alternative button views
    Add constructors for nested KeyButtonInfo class to allow
    extending in special-case keybuttonview classes
    
    Change-Id: Ide7e850f2fc2cade6105e049df024112e9e3b4a9

--- packages/SystemUI/src/com/android/systemui/statusbar/policy/LayoutChangerButtonView.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/policy/LayoutChangerButtonView.java
@@ -20,10 +20,7 @@ import android.animation.Animator;
 import android.animation.AnimatorSet;
 import android.animation.ObjectAnimator;
 import android.content.Context;
-import android.graphics.Canvas;
-import android.graphics.RectF;
-import android.graphics.drawable.BitmapDrawable;
-import android.graphics.drawable.Drawable;
+import android.content.res.Resources;
 import android.util.AttributeSet;
 import android.util.Log;
 import android.view.HapticFeedbackConstants;
@@ -32,7 +29,6 @@ import android.view.SoundEffectConstants;
 import android.view.View;
 import android.view.ViewConfiguration;
 import android.view.accessibility.AccessibilityEvent;
-import android.widget.ImageView;
 
 import com.android.internal.util.aokp.AwesomeAction;
 import com.android.internal.util.aokp.NavBarHelpers;
@@ -40,26 +36,22 @@ import com.android.internal.util.vanir.AwesomeConstants;
 import com.android.internal.util.vanir.AwesomeConstants.AwesomeConstant;
 import com.android.systemui.R;
 
-public class LayoutChangerButtonView extends ImageView {
+public class LayoutChangerButtonView extends KeyButtonView {
     private static final String TAG = "StatusBar.LayoutChangerButtonView";
     public static final String ACTION_MENU = AwesomeConstant.ACTION_MENU.value();
     public static final String LAYOUT_RIGHT = AwesomeConstant.ACTION_LAYOUT_RIGHT.value();
 
     final float GLOW_MAX_SCALE_FACTOR = 1.8f;
-    public static final float DEFAULT_QUIESCENT_ALPHA = 0.25f;
+    public static final float LAYOUT_CHANGER_QUIESCENT_ALPHA = 0.30f;
 
     int mTouchSlop;
-    Drawable mGlowBG;
-    int mGlowWidth, mGlowHeight;
-    float mGlowAlpha = 0f, mGlowScale = 1f;
+    final float mQuiescentAlpha = LAYOUT_CHANGER_QUIESCENT_ALPHA;
     float mDrawingAlpha = 1f;
-    float mQuiescentAlpha = DEFAULT_QUIESCENT_ALPHA;
-    RectF mRect = new RectF();
     AnimatorSet mPressedAnim;
     Animator mAnimateToQuiescent = new ObjectAnimator();
     AnimatorSet as = mPressedAnim = new AnimatorSet();
 
-    LayoutButtonInfo mActions;
+    KeyButtonInfo mActions;
 
     public LayoutChangerButtonView(Context context, AttributeSet attrs) {
         this(context, attrs, 0);
@@ -69,19 +61,16 @@ public class LayoutChangerButtonView extends ImageView {
         super(context, attrs);
 
         setDrawingAlpha(mQuiescentAlpha);
-        if (mGlowBG != null) {
-            mGlowWidth = mGlowBG.getIntrinsicWidth();
-            mGlowHeight = mGlowBG.getIntrinsicHeight();
-        }
 
         setClickable(true);
         mTouchSlop = ViewConfiguration.get(context).getScaledTouchSlop();
         setLongClickable(false);
     }
 
-    public void setButtonActions(LayoutButtonInfo actions) {
+    @Override
+    public void setButtonActions(KeyButtonInfo actions) {
         this.mActions = actions;
-        setTag(mActions.singleAction); // should be OK even if it's null
+        setTag(mActions.singleAction);
         setImage();
     }
 
@@ -95,42 +84,16 @@ public class LayoutChangerButtonView extends ImageView {
         }
     }
 
+    @Override
     public void setImage() {
         setImageDrawable(NavBarHelpers.getIconImage(mContext, LAYOUT_RIGHT));
     }
 
     @Override
-    protected void onDraw(Canvas canvas) {
-        if (mGlowBG != null) {
-            canvas.save();
-            final int w = getWidth();
-            final int h = getHeight();
-            final float aspect = (float) mGlowWidth / mGlowHeight;
-            final int drawW = (int) (h * aspect);
-            final int drawH = h;
-            final int margin = (drawW - w) / 2;
-            canvas.scale(mGlowScale, mGlowScale, w * 0.5f, h * 0.5f);
-            mGlowBG.setBounds(-margin, 0, drawW - margin, drawH);
-            mGlowBG.setAlpha((int) (mDrawingAlpha * mGlowAlpha * 255));
-            mGlowBG.draw(canvas);
-            canvas.restore();
-            mRect.right = w;
-            mRect.bottom = h;
-        }
-        super.onDraw(canvas);
-    }
+    public void setImage(final Resources res) { }
 
     public void setQuiescentAlpha(float alpha, boolean animate) {
-        mAnimateToQuiescent.cancel();
-        alpha = Math.min(Math.max(alpha, 0), 1);
-        if (alpha == mQuiescentAlpha && alpha == mDrawingAlpha) return;
-        mQuiescentAlpha = alpha;
-        if (mGlowBG != null && animate) {
-            mAnimateToQuiescent = animateToQuiescent();
-            mAnimateToQuiescent.start();
-        } else {
-            setDrawingAlpha(mQuiescentAlpha);
-        }
+        setDrawingAlpha(mQuiescentAlpha);
     }
 
     private ObjectAnimator animateToQuiescent() {
@@ -148,77 +111,6 @@ public class LayoutChangerButtonView extends ImageView {
     public void setDrawingAlpha(float x) {
         setAlpha((int) (x * 255));
         mDrawingAlpha = x;
-    }
-
-    public float getGlowAlpha() {
-        if (mGlowBG == null) return 0;
-        return mGlowAlpha;
-    }
-
-    public void setGlowAlpha(float x) {
-        if (mGlowBG == null) return;
-        mGlowAlpha = x;
-        invalidate();
-    }
-
-    public float getGlowScale() {
-        if (mGlowBG == null) return 0;
-        return mGlowScale;
-    }
-
-    public void setGlowScale(float x) {
-        if (mGlowBG == null) return;
-        mGlowScale = x;
-        final float w = getWidth();
-        final float h = getHeight();
-        if (GLOW_MAX_SCALE_FACTOR <= 1.0f) {
-            // this only works if we know the glow will never leave our bounds
-            invalidate();
-        } else {
-            final float rx = (w * (GLOW_MAX_SCALE_FACTOR - 1.0f)) / 2.0f + 1.0f;
-            final float ry = (h * (GLOW_MAX_SCALE_FACTOR - 1.0f)) / 2.0f + 1.0f;
-            com.android.systemui.SwipeHelper.invalidateGlobalRegion(
-                    this,
-                    new RectF(getLeft() - rx,
-                            getTop() - ry,
-                            getRight() + rx,
-                            getBottom() + ry));
-
-            if (getParent() != null) ((View) getParent()).invalidate();
-        }
-    }
-
-    public void setPressed(boolean pressed) {
-        if (mGlowBG != null) {
-            if (pressed != isPressed()) {
-                if (mPressedAnim != null && mPressedAnim.isRunning()) {
-                    mPressedAnim.cancel();
-                }
-                if (pressed) {
-                    if (mGlowScale < GLOW_MAX_SCALE_FACTOR)
-                        mGlowScale = GLOW_MAX_SCALE_FACTOR;
-                    if (mGlowAlpha < mQuiescentAlpha)
-                        mGlowAlpha = mQuiescentAlpha;
-                    setDrawingAlpha(1f);
-                    as.playTogether(
-                            ObjectAnimator.ofFloat(this, "glowAlpha", 1f),
-                            ObjectAnimator.ofFloat(this, "glowScale", GLOW_MAX_SCALE_FACTOR)
-                    );
-                    as.setDuration(50);
-                } else {
-                    mAnimateToQuiescent.cancel();
-                    mAnimateToQuiescent = animateToQuiescent();
-                    as.playTogether(
-                            ObjectAnimator.ofFloat(this, "glowAlpha", 0f),
-                            ObjectAnimator.ofFloat(this, "glowScale", 1f),
-                            mAnimateToQuiescent
-                    );
-                    as.setDuration(500);
-                }
-                as.start();
-            }
-        }
-        super.setPressed(pressed);
     }
 
     public boolean onTouchEvent(MotionEvent ev) {
@@ -240,11 +132,10 @@ public class LayoutChangerButtonView extends ImageView {
                 setPressed(false);
                 break;
             case MotionEvent.ACTION_UP:
-                boolean playSound = isPressed();
-                setPressed(false);
-                if (playSound) {
+                if (isPressed()) {
                     playSoundEffect(SoundEffectConstants.CLICK);
                 }
+                setPressed(false);
                 doSinglePress();
                 break;
         }
@@ -257,22 +148,5 @@ public class LayoutChangerButtonView extends ImageView {
         }
         AwesomeAction.launchAction(mContext, mActions.singleAction);
         sendAccessibilityEvent(AccessibilityEvent.TYPE_VIEW_CLICKED);
-    }
-
-    public void setGlowBackground(int resId) {
-        mGlowBG = getResources().getDrawable(resId);
-        if (mGlowBG != null) {
-            setDrawingAlpha(mDrawingAlpha);
-            mGlowWidth = mGlowBG.getIntrinsicWidth();
-            mGlowHeight = mGlowBG.getIntrinsicHeight();
-        }
-    }
-
-    public static class LayoutButtonInfo {
-        String singleAction;
-
-        public LayoutButtonInfo(String singleTap) {
-            this.singleAction = singleTap;
-        }
     }
 }
