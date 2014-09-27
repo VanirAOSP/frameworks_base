42a153965b2335df288bfe2afeedf22e63329cfe
Randall Rushing <randall.rushing@gmail.com>
2014-09-27 00:13:53 -0700

    SystemUI: configure softkey long press timeout (1 of 2) WIP
    Change-Id: I5241ad486b4025ed01ea1b0feefeaec4fb0dfa25

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/NavigationBarView.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/NavigationBarView.java
@@ -48,6 +48,7 @@ import android.view.Gravity;
 import android.view.MotionEvent;
 import android.view.Surface;
 import android.view.View;
+import android.view.ViewConfiguration;
 import android.view.ViewGroup;
 import android.view.WindowManager;
 import android.view.accessibility.AccessibilityManager;
@@ -66,6 +67,7 @@ import com.android.systemui.statusbar.policy.DeadZone;
 import com.android.systemui.statusbar.policy.LayoutChangerButtonView;
 import com.android.systemui.statusbar.policy.KeyButtonView;
 import com.android.systemui.statusbar.policy.KeyButtonView.KeyButtonInfo;
+import com.vanir.util.Helpers;
 
 import java.io.FileDescriptor;
 import java.io.PrintWriter;
@@ -74,6 +76,8 @@ import java.util.ArrayList;
 public class NavigationBarView extends LinearLayout {
     final static boolean DEBUG = false;
     final static String TAG = "PhoneStatusBar/NavigationBarView";
+
+    private static final String URI_SOFTKEY_LONGPRESS_TIMEOUT = "vanir_softkey_longpress_timeout";
 
     final static boolean NAVBAR_ALWAYS_AT_RIGHT = true;
 
@@ -718,6 +722,14 @@ public class NavigationBarView extends LinearLayout {
                                     Settings.System.ACTIVE_NOTIFICATIONS_PRIVACY_MODE, 0) == 0;
                     mPrefNavring = Settings.System.getInt(r,
                             Settings.System.ENABLE_NAVIGATION_RING, 1) == 1;
+
+                    final int defLongPressTimeout = ViewConfiguration.getLongPressTimeout();
+                    int longPressTimeout = Settings.System.getInt(r,
+                            URI_SOFTKEY_LONGPRESS_TIMEOUT, defLongPressTimeout);
+                    for (KeyButtonView v : Helpers.getAllChildren(NavigationBarView.this,
+                            KeyButtonView.class)) {
+                        v.setLongPressTimeout(longPressTimeout);
+                    }
                 }};
 
             r.registerContentObserver(Settings.System.getUriFor(Settings.System.LOCKSCREEN_NOTIFICATIONS),
@@ -727,6 +739,8 @@ public class NavigationBarView extends LinearLayout {
             r.registerContentObserver(Settings.System.getUriFor(Settings.System.ACTIVE_NOTIFICATIONS),
                     false, mDisablePrefsObserver);
             r.registerContentObserver(Settings.System.getUriFor(Settings.System.ACTIVE_NOTIFICATIONS_PRIVACY_MODE),
+                    false, mDisablePrefsObserver);
+            r.registerContentObserver(Settings.System.getUriFor(URI_SOFTKEY_LONGPRESS_TIMEOUT),
                     false, mDisablePrefsObserver);
 
             // pop goes the weasel
