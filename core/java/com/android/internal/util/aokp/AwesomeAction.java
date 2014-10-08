0aabba890721dec0c05f526a12e02bcd94b6a7fd
nuclearmistake <nuclearmistake@gmail.com>
2014-10-08 17:25:14 -0400

    On hardware key devices, ACTION_APP hardware buttons block the input threat... for stuff like Camera, that's a LOONNNGGG time to block.
    Change-Id: Ibd498b3aa82eaccf200b02f0be823d2a50bcc7d5

--- core/java/com/android/internal/util/aokp/AwesomeAction.java
+++ core/java/com/android/internal/util/aokp/AwesomeAction.java
@@ -101,7 +101,6 @@ public class AwesomeAction {
                 break;
 
             case ACTION_KILL:
-                mHandler.removeCallbacksAndMessages(null);
                 mHandler.post(new Runnable() {
                     @Override
                     public void run() {
@@ -148,15 +147,19 @@ public class AwesomeAction {
                 break;
 
             case ACTION_APP:
-                try {
-                    Intent intentapp = Intent.parseUri(action, 0);
-                    intentapp.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
-                    mContext.startActivity(intentapp);
-                } catch (URISyntaxException e) {
-                    Log.e(TAG, "URISyntaxException: [" + action + "]");
-                } catch (ActivityNotFoundException e) {
-                    Log.e(TAG, "ActivityNotFound: [" + action + "]");
-                }
+                mHandler.post(new Runnable() {
+                    @Override
+                    public void run() {
+                        try {
+                            Intent intentapp = Intent.parseUri(action, 0);
+                            intentapp.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                            mContext.startActivity(intentapp);
+                        } catch (URISyntaxException e) {
+                            Log.e(TAG, "URISyntaxException: [" + action + "]");
+                        } catch (ActivityNotFoundException e) {
+                            Log.e(TAG, "ActivityNotFound: [" + action + "]");
+                        }
+                }});
                 break;
 
             case ACTION_APP_WINDOW:
