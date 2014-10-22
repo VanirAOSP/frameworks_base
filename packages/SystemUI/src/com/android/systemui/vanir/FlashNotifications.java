0584c1df45533a6836008cef0fa846b3fb04ef87
PrimeDirective <activethrasher00@gmail.com>
2014-10-21 21:10:12 -0400

    [WIP] Flash notifications [1/2]
    Flash the LED when a notification is received
    
    works well enough for alpha testing.  currently needs settings
    configurability and a runnable
    
    Change-Id: I738b7dab5f0aa942023d812776f0c8a62ea5e8d9

---
+++ packages/SystemUI/src/com/android/systemui/vanir/FlashNotifications.java
@@ -0,0 +1,249 @@
+/*
+ * Copyright (C) 2014 VanirAOSP && the Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.android.systemui.vanir;
+
+import android.app.ActivityManagerNative;
+import android.app.AlarmManager;
+import android.app.INotificationManager;
+import android.app.KeyguardManager;
+import android.app.Notification;
+import android.app.PendingIntent;
+import android.app.PendingIntent.CanceledException;
+import android.app.StatusBarManager;
+import android.content.BroadcastReceiver;
+import android.content.ComponentName;
+import android.content.ContentResolver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.content.pm.PackageManager;
+import android.content.pm.PackageManager.NameNotFoundException;
+import android.content.res.Configuration;
+import android.content.res.Resources;
+import android.graphics.SurfaceTexture;
+import android.hardware.Camera;
+import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
+import android.os.PowerManager;
+import android.os.RemoteException;
+import android.os.ServiceManager;
+import android.os.SystemClock;
+import android.os.UserHandle;
+import android.provider.Settings;
+import android.service.notification.INotificationListener;
+import android.service.notification.StatusBarNotification;
+import android.telephony.TelephonyManager;
+import android.text.TextUtils;
+import android.util.AttributeSet;
+import android.util.Log;
+
+import com.android.internal.util.cm.TorchConstants;
+
+import java.io.IOException;
+import java.util.Calendar;
+import java.util.HashSet;
+import java.util.Set;
+
+public class FlashNotifications {
+    private static final String TAG = "FlashNotifications";
+    private static final boolean DEBUG = false;
+
+    private static final int HIDE_NOTIFICATIONS_BELOW_SCORE = Notification.PRIORITY_LOW;
+    private static final int FLASH_COUNT = 4;
+
+    Camera cam;
+    Camera.Parameters params;
+    TelephonyManager mTM;
+    INotificationManager mNM;
+    INotificationListenerWrapper mNotificationListener;
+
+    Context mContext;
+    private boolean mAble;
+    volatile boolean mFlashing = false;
+    volatile boolean mLightOn = false;
+    boolean mGlobalTorchOn = false;
+
+    // to be made configurable
+    boolean mQuietTime = false;
+    int mFlashTimeout = 50;
+    boolean mScreenIsOn = true;
+    boolean hideNonClearable = true;
+    boolean mHideLowPriorityNotifications = false;
+
+    private Set<String> mExcludedApps = new HashSet<String>();
+
+    private static final String[] AUTO_BANNED_PACKAGES = new String[] {
+    //    "android",   unblock to use for for testing
+        "com.android.music",
+        "net.cactii.flash2",
+        "com.andrew.apollo",
+        "com.google.android.music",
+        "com.android.providers.downloads"
+    };
+
+    /**
+     * Listen to notification events here:
+     */
+    private class INotificationListenerWrapper extends INotificationListener.Stub {
+        @Override
+        public void onNotificationPosted(final StatusBarNotification sbn) {
+            if (mQuietTime) {
+                if (inQuietHours()) return;
+            }
+            if (!mFlashing && isValidNotification(sbn) && !mScreenIsOn && !mGlobalTorchOn) {
+                Intent torch = new Intent(TorchConstants.ACTION_TOGGLE_STATE);
+                torch.addFlags(Intent.FLAG_RECEIVER_FOREGROUND);
+                torch.putExtra("flash_notification", true);
+                mFlashing = true;
+                synchronized(this) {
+                    for (int i = 0; i < FLASH_COUNT; i++) {
+                        mContext.sendBroadcast(torch);
+                        mLightOn = !mLightOn;
+                        if (mLightOn) {
+                            mFlashTimeout = 50;
+                        } else {
+                            mFlashTimeout = 150;
+                        }
+                        try {
+                            Thread.sleep(mFlashTimeout);
+                        } catch (InterruptedException InternetExplorer) {
+                        }
+                    }
+                    mFlashing = false;
+                }
+            }
+        }
+        @Override public void onNotificationRemoved(final StatusBarNotification sbn) {
+            // not used yet
+        }
+    }
+
+    private final BroadcastReceiver mBroadcastReceiver = new BroadcastReceiver() {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            String action = intent.getAction();
+            if (TorchConstants.ACTION_STATE_CHANGED.equals(action)) {
+                mGlobalTorchOn = intent.getIntExtra(TorchConstants.EXTRA_CURRENT_STATE, 0) != 0;
+            } else if (Intent.ACTION_SCREEN_OFF.equals(action)) {
+                Log.i(TAG, "SCREEN OFF");
+                mScreenIsOn = false;
+            } else if (Intent.ACTION_SCREEN_ON.equals(action)) {
+                mScreenIsOn = true;
+            }
+        }
+    };
+
+    public FlashNotifications(Context context) {
+        mContext = context;
+
+        mNM = INotificationManager.Stub.asInterface(
+                    ServiceManager.getService(Context.NOTIFICATION_SERVICE));
+        mNotificationListener = new INotificationListenerWrapper();
+        mTM = (TelephonyManager) mContext.getSystemService(Context.TELEPHONY_SERVICE);
+
+        final boolean isFlashSupported = mContext.getPackageManager().hasSystemFeature(PackageManager.FEATURE_CAMERA_FLASH);
+        final boolean isCameraPresent = mContext.getPackageManager().hasSystemFeature(PackageManager.FEATURE_CAMERA);
+        mAble = (isFlashSupported && isCameraPresent);
+
+        if (mAble) registerListeners();
+    }
+
+
+    /**
+     * Determine if a given notification should be used.
+     * @param sbn StatusBarNotification to check.
+     * @return True if it should be used, false otherwise.
+     */
+    private boolean isValidNotification(StatusBarNotification sbn) {
+/*        if (isOnCall() || mExcludedApps.contains(sbn.getPackageName())) return false; */
+        for (String packageName : AUTO_BANNED_PACKAGES) {
+            if (packageName.equals(sbn.getPackageName())) {
+                 Log.i(TAG, "Package name: " + sbn.getPackageName() + " is banned. Not a valid notification.");
+                return false;
+            }
+        }
+
+/*        return ((sbn.isClearable() || !hideNonClearable)
+                && (!mHideLowPriorityNotifications || sbn.getNotification().priority > HIDE_NOTIFICATIONS_BELOW_SCORE)
+                && sbn.getNotification().icon != 0);
+*/        return true;
+    }
+
+    private void registerListeners() {
+        ComponentName cn = new ComponentName("android", "");
+        try {
+            mNM.registerListener(mNotificationListener, cn, UserHandle.USER_ALL);
+        } catch (RemoteException e) {
+            Log.e(TAG, "registerNotificationListener()", e);
+        }
+        IntentFilter filter = new IntentFilter();
+        filter.addAction(Intent.ACTION_SCREEN_OFF);
+        filter.addAction(Intent.ACTION_SCREEN_ON);
+        filter.addAction(TorchConstants.ACTION_STATE_CHANGED);
+        mContext.registerReceiver(mBroadcastReceiver, filter);
+    }
+
+    public void unregisterNotificationListener() {
+        if (mNotificationListener !=  null) {
+            try {
+                mNM.unregisterListener(mNotificationListener, UserHandle.USER_ALL);
+            } catch (RemoteException e) {
+                Log.e(TAG, "registerNotificationListener()", e);
+            }
+        }
+        if (mBroadcastReceiver != null) mContext.unregisterReceiver(mBroadcastReceiver);
+    }
+
+    /**
+     * Determine i a call is currently in progress.
+     * @return True if a call is in progress.
+     */
+    protected boolean isOnCall() {
+        if (mTM == null)
+            mTM = (TelephonyManager) mContext.getSystemService(Context.TELEPHONY_SERVICE);
+        return mTM.getCallState() != TelephonyManager.CALL_STATE_IDLE;
+    }
+
+    /**
+     * Check if device is in Quiet Hours in the moment.
+     */
+    protected boolean inQuietHours() {
+        boolean quietHoursEnabled = Settings.System.getIntForUser(mContext.getContentResolver(),
+                Settings.System.QUIET_HOURS_ENABLED, 0, UserHandle.USER_CURRENT_OR_SELF) != 0;
+        if (quietHoursEnabled) {
+            int quietHoursStart = Settings.System.getIntForUser(mContext.getContentResolver(),
+                    Settings.System.QUIET_HOURS_START, 0, UserHandle.USER_CURRENT_OR_SELF);
+            int quietHoursEnd = Settings.System.getIntForUser(mContext.getContentResolver(),
+                    Settings.System.QUIET_HOURS_END, 0, UserHandle.USER_CURRENT_OR_SELF);
+            boolean quietHoursDim = Settings.System.getIntForUser(mContext.getContentResolver(),
+                    Settings.System.QUIET_HOURS_DIM, 0, UserHandle.USER_CURRENT_OR_SELF) != 0;
+
+            if (quietHoursDim && (quietHoursStart != quietHoursEnd)) {
+                Calendar calendar = Calendar.getInstance();
+                int minutes = calendar.get(Calendar.HOUR_OF_DAY) * 60 + calendar.get(Calendar.MINUTE);
+                if (quietHoursEnd < quietHoursStart) {
+                    return (minutes > quietHoursStart) || (minutes < quietHoursEnd);
+                } else {
+                    return (minutes > quietHoursStart) && (minutes < quietHoursEnd);
+                }
+            }
+        }
+        return false;
+    }
+}
+
+    
