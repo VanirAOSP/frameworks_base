7f4ecfc753843a03776bc5be04c8138e5918da6b
PrimeDirective <activethrasher00@gmail.com>
2014-10-25 15:11:16 -0400

    ActiveDisplay: don't get call status while checking for valid notifications
    isValidNotification(...) is called at times other than when we're checking
    to see if we need to show active display so lets check for call status at that
    time only
    
    Change-Id: I59177f538cee572f9591af89d175b36530041944

--- packages/SystemUI/src/com/android/systemui/statusbar/policy/activedisplay/ActiveDisplayView.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/policy/activedisplay/ActiveDisplayView.java
@@ -172,10 +172,8 @@ public class ActiveDisplayView extends FrameLayout {
     private class INotificationListenerWrapper extends INotificationListener.Stub {
         @Override
         public void onNotificationPosted(final StatusBarNotification sbn) {
-            if (mQuietTime) {
-                if (inQuietHours()) return;
-            }
             synchronized (this) {
+                if (isOnCall() || (mQuietTime && inQuietHours())) return;
                 if (shouldShowNotificationForPocketMode() && isValidNotification(sbn)) {
                     // need to make sure either the screen is off or the user is currently
                     // viewing the notifications
@@ -897,7 +895,7 @@ public class ActiveDisplayView extends FrameLayout {
      * @return True if it should be used, false otherwise.
      */
     protected boolean isValidNotification(StatusBarNotification sbn) {
-        if (isOnCall() || mExcludedPackages.contains(sbn.getPackageName())) return false;
+        if (mExcludedPackages.contains(sbn.getPackageName())) return false;
 
         return ((sbn.isClearable() || !hideNonClearable)
                 && (!mHideLowPriorityNotifications || sbn.getNotification().priority > HIDE_NOTIFICATIONS_BELOW_SCORE)
@@ -1035,10 +1033,8 @@ public class ActiveDisplayView extends FrameLayout {
             if (event.sensor.equals(mProximitySensor)) {
                 if (value >= mProximitySensor.getMaximumRange()) {
                     mDistanceFar = true;
-                    if (mQuietTime) {
-                        if (inQuietHours()) return;
-                    }
                     synchronized (this) {
+                        if (mQuietTime && inQuietHours()) return;
                         if (!mScreenOnState) {
                             if (checkTime >= (mPocketTime + mProximityThreshold)){
                                 if (mNotification == null) {
@@ -1068,10 +1064,8 @@ public class ActiveDisplayView extends FrameLayout {
         public void onReceive(Context context, Intent intent) {
             String action = intent.getAction();
             if (ACTION_REDISPLAY_NOTIFICATION.equals(action)) {
-                if (mQuietTime) {
-                    if (inQuietHours()) return;
-                }
                 synchronized (this) {
+                    if (isOnCall() || (mQuietTime && inQuietHours())) return;
                     if (!mScreenOnState) {
                         if (mNotification == null) {
                             mNotification = getNextAvailableNotification();
