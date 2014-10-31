f51528201f0044122d3ed6699646acff553ab2e5
Dave <activethrasher00@gmail.com>
2014-10-31 19:40:05 -0400

    Fix NPE on tablets for notification navbar action
    QS side doesn't ever add the button it looks for to determine shade state
    because it doesnt' have "flip" settings
    
    Change-Id: If47bb6505616b5d98f3eebf656da1435f29f7cb2

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBar.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBar.java
@@ -296,6 +296,7 @@ public class PhoneStatusBar extends BaseStatusBar implements DemoMode,
     // position
     int[] mPositionTmp = new int[2];
     boolean mExpandedVisible;
+    boolean mNotificationStateNext = true;
 
     // the date view
     DateView mDateView;
@@ -2264,13 +2265,23 @@ public class PhoneStatusBar extends BaseStatusBar implements DemoMode,
      */
     private int getExpandedNotificationState() {
         if (mExpandedVisible) {
-            // Notification button is on quick settings side
-            if (mNotificationButton.getVisibility() == View.VISIBLE) {
-                return 1;
-            // Settings button is on notification side
-            } else if (mSettingsButton.getVisibility() == View.VISIBLE) {
-                return 2;
-            }
+			if (mNotificationButton != null) {
+				// Notification button is on quick settings side
+				if (mNotificationButton.getVisibility() == View.VISIBLE) {
+					return 1;
+				// Settings button is on notification side
+				} else if (mSettingsButton.getVisibility() == View.VISIBLE) {
+					return 2;
+				}
+			} else {
+				if (mNotificationStateNext) {
+					mNotificationStateNext = false;
+					return 2;
+				} else {
+					mNotificationStateNext = true;
+					return 1;
+				}
+			}
         }
         return 0;
     }
