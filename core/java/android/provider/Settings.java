832052ec6fa5dc68e83f059848cdffc8d1f9ffa1
Lars Greiss <kufikugel@googlemail.com>
2014-12-06 17:23:35 +0100

    Enable Quick Unlock (2/2)
    old way does not work anymore exactly like before. So adapt the idea to new LP code
    
    Thankd DVTonder for the basic idea prior LP
    
    PS:
    - do PIN view correctly
    
    Change-Id: I86660afafb8648d36d6c135343c88afcfc5173fc

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -5548,7 +5548,11 @@ public final class Settings {
          * @hide
          */
         public static final String SMS_DEFAULT_APPLICATION = "sms_default_application";
-
+        /**
+         * Whether to use the custom quick unlock screen control
+         * @hide
+         */
+        public static final String LOCKSCREEN_QUICK_UNLOCK_CONTROL = "lockscreen_quick_unlock_control";
         /**
          * Name of a package that the current user has explicitly allowed to see all of that
          * user's notifications.
@@ -5729,6 +5733,7 @@ public final class Settings {
             PRIVACY_GUARD_DEFAULT,
             ADVANCED_REBOOT,
             DEVELOPMENT_SHORTCUT
+            LOCKSCREEN_QUICK_UNLOCK_CONTROL
         };
 
         /**
