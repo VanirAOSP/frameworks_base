9541f27a254b3aa9d3edcd1fc53eaa6d5d21a6c6
Diogo Ferreira <defer@cyngn.com>
2014-12-17 17:16:16 +0000

    su: Add an indicator when a 'su' session is active
    This adds an indicator to the statusbar that is visible whenever a root
    session is active, akin to the fine location icon.
    
    Change-Id: I669fcc87e2b7a3201308170d7727ec5fb0e00e2b
    Signed-off-by: DHOMD <DHO@dho.im>
    
    Conflicts:
    	services/core/java/com/android/server/AppOpsService.java

--- services/core/java/com/android/server/AppOpsService.java
+++ services/core/java/com/android/server/AppOpsService.java
@@ -36,6 +36,7 @@ import android.app.ActivityThread;
 import android.app.AppOpsManager;
 import android.app.Dialog;
 import android.content.Context;
+import android.content.Intent;
 import android.content.pm.ApplicationInfo;
 import android.content.pm.IPackageManager;
 import android.content.pm.PackageManager;
@@ -231,6 +232,13 @@ public class AppOpsService extends IAppOpsService.Stub {
                     finishOperationLocked(mStartedOps.get(i));
                 }
                 mClients.remove(mAppToken);
+            }
+
+            // We cannot broadcast on the synchronized block above because the broadcast might
+            // trigger another appop call that eventually arrives here from a different thread,
+            // causing a deadlock.
+            for (int i=mStartedOps.size()-1; i>=0; i--) {
+                broadcastOpIfNeeded(mStartedOps.get(i).op);
             }
         }
     }
@@ -723,6 +731,7 @@ public class AppOpsService extends IAppOpsService.Stub {
                 op.time = System.currentTimeMillis();
                 op.rejectTime = 0;
                 op.allowedCount++;
+                broadcastOpIfNeeded(code);
                 return AppOpsManager.MODE_ALLOWED;
             } else {
                 if (Looper.myLooper() == mLooper) {
@@ -739,7 +748,10 @@ public class AppOpsService extends IAppOpsService.Stub {
                 req = askOperationLocked(code, uid, packageName, switchOp);
             }
         }
-        return req.get();
+
+        int result = req.get();
+        broadcastOpIfNeeded(code);
+        return result;
     }
 
     @Override
@@ -787,6 +799,7 @@ public class AppOpsService extends IAppOpsService.Stub {
                 if (client.mStartedOps != null) {
                     client.mStartedOps.add(op);
                 }
+                broadcastOpIfNeeded(code);
                 return AppOpsManager.MODE_ALLOWED;
             } else {
                 if (Looper.myLooper() == mLooper) {
@@ -805,7 +818,9 @@ public class AppOpsService extends IAppOpsService.Stub {
                 req = askOperationLocked(code, uid, packageName, switchOp);
             }
         }
-        return req.get();
+        int result = req.get();
+        broadcastOpIfNeeded(code);
+        return result;
     }
 
     @Override
@@ -826,6 +841,7 @@ public class AppOpsService extends IAppOpsService.Stub {
             }
             finishOperationLocked(op);
         }
+        broadcastOpIfNeeded(code);
     }
 
     void finishOperationLocked(Op op) {
