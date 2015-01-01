ea41e2434b86e6aa66ace62b1c1b7d8c8b0a8cfa
Dave Kessler <activethrasher00@gmail.com>
2014-12-31 21:05:19 -0500

    Revert "Squashed commit of TorchService from cm-11.0"
    This reverts commit 42f3a0d460b9a4af5590b5bd785d71253bec3e97.

--- core/java/android/hardware/Camera.java
+++ core/java/android/hardware/Camera.java
@@ -20,13 +20,11 @@ import android.app.ActivityThread;
 import android.annotation.SdkConstant;
 import android.annotation.SdkConstant.SdkConstantType;
 import android.content.Context;
-import android.hardware.ITorchService;
 import android.graphics.ImageFormat;
 import android.graphics.Point;
 import android.graphics.Rect;
 import android.graphics.SurfaceTexture;
 import android.media.IAudioService;
-import android.os.Binder;
 import android.os.Handler;
 import android.os.IBinder;
 import android.os.Looper;
@@ -161,8 +159,6 @@ public class Camera {
     private static final int CAMERA_MSG_META_DATA        = 0x2000;
     /* ### QC ADD-ONS: END */
 
-    private int mCameraId;
-
     private long mNativeContext; // accessed by native methods
     private EventHandler mEventHandler;
     private ShutterCallback mShutterCallback;
@@ -193,7 +189,6 @@ public class Camera {
     private CameraDataCallback mCameraDataCallback;
     private CameraMetaDataCallback mCameraMetaDataCallback;
     /* ### QC ADD-ONS: END */
-    private Binder mTorchToken;
 
     /**
      * Broadcast Action:  A new picture is taken by the camera, and the entry of
@@ -468,7 +463,6 @@ public class Camera {
     }
 
     private int cameraInitVersion(int cameraId, int halVersion) {
-        mCameraId = cameraId;
         mShutterCallback = null;
         mRawImageCallback = null;
         mJpegCallback = null;
@@ -480,7 +474,6 @@ public class Camera {
         mCameraDataCallback = null;
         mCameraMetaDataCallback = null;
         /* ### QC ADD-ONS: END */
-        mTorchToken = new Binder();
 
         Looper looper;
         if ((looper = Looper.myLooper()) != null) {
@@ -492,7 +485,6 @@ public class Camera {
         }
 
         String packageName = ActivityThread.currentPackageName();
-        notifyTorch(true);
 
         return native_setup(new WeakReference<Camera>(this), cameraId, halVersion, packageName);
     }
@@ -556,20 +548,6 @@ public class Camera {
     Camera() {
     }
 
-    private void notifyTorch(boolean inUse) {
-        IBinder b = ServiceManager.getService(Context.TORCH_SERVICE);
-        ITorchService torchService = ITorchService.Stub.asInterface(b);
-        try {
-            if (inUse) {
-                torchService.onCameraOpened(mTorchToken, mCameraId);
-            } else {
-                torchService.onCameraClosed(mTorchToken, mCameraId);
-            }
-        } catch (RemoteException e) {
-            // Ignore
-        }
-    }
-
     @Override
     protected void finalize() {
         release();
@@ -589,7 +567,6 @@ public class Camera {
     public final void release() {
         native_release();
         mFaceDetectionRunning = false;
-        notifyTorch(false);
     }
 
     /**
