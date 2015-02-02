48c32d028da00f6cc25665d10a3c5b3f79f5bf4c
Danny Baumann <dannybaumann@web.de>
2015-02-02 17:31:38 +0100

    Remove pointless notification drawer network info label in MSIM mode.
    Change-Id: I44761c152094ae71050cbb7faf9ee1671b6c2dc4

--- packages/SystemUI/src/com/android/systemui/statusbar/policy/MSimNetworkControllerImpl.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/policy/MSimNetworkControllerImpl.java
@@ -42,6 +42,7 @@ import android.provider.Telephony;
 import android.telephony.PhoneStateListener;
 import android.telephony.ServiceState;
 import android.telephony.SignalStrength;
+import android.telephony.SubInfoRecord;
 import android.telephony.SubscriptionManager;
 import android.telephony.SubInfoRecord;
 import android.telephony.TelephonyManager;
@@ -109,7 +110,6 @@ public class MSimNetworkControllerImpl extends NetworkControllerImpl {
     int PHONE_ID2 = PhoneConstants.PHONE_ID2;
     private HashMap<Long, Integer> mSubIdPhoneIdMap;
     ArrayList<MSimSignalCluster> mSimSignalClusters = new ArrayList<MSimSignalCluster>();
-    ArrayList<TextView> mSubsLabelViews = new ArrayList<TextView>();
 
     public interface MSimSignalCluster {
         void setWifiIndicators(boolean visible, int strengthIcon, int activityIcon,
@@ -449,14 +449,6 @@ public class MSimNetworkControllerImpl extends NetworkControllerImpl {
         }
     }
 
-    public void addSubsLabelView(TextView v) {
-        mSubsLabelViews.add(v);
-    }
-
-    public void clearSubsLabelView() {
-        mSubsLabelViews.clear();
-    }
-
     private void updateCarrierText(int sub) {
         int textResId = 0;
         if (mAirplaneMode) {
@@ -506,17 +498,10 @@ public class MSimNetworkControllerImpl extends NetworkControllerImpl {
             carrierName = carrierName + "    " + mCarrierTextSub[i];
         }
 
-        if (mContext.getResources().getBoolean(R.bool.config_showDataConnectionView)) {
-            for (int i = 0; i < mSubsLabelViews.size(); i++) {
-                TextView v = mSubsLabelViews.get(i);
-                v.setText(carrierName);
-            }
-        } else {
-            for (int i = 0; i < mMobileLabelViews.size(); i++) {
-                TextView v = mMobileLabelViews.get(i);
-                v.setText(carrierName);
-                v.setVisibility(View.VISIBLE);
-            }
+        for (int i = 0; i < mMobileLabelViews.size(); i++) {
+            TextView v = mMobileLabelViews.get(i);
+            v.setText(carrierName);
+            v.setVisibility(View.VISIBLE);
         }
     }
 
