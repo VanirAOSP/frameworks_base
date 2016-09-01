e05224232a9b0d6db6c56ed6c9b2444d4cef677b
nuclearmistake <nuclearmistake@gmail.com>
2016-09-01 17:48:21 -0400

    aapt: suppress string resources build warnings
    warnings suppressed are:
    aapt: warning: **** string '%s' has no default or required localization for
    aapt: warning: string '%s' has no default translation in
    
    Patch Set 2: move ifdef and only suppress warnings for packages that
                 define NO_DEFAULT_TRANSLATION_WARNINGS and/or
                 NO_LOCALIZATION_WARNING
    
    Patch Set 3: by default do not show warnings unless
                 SHOW_DEFAULT_TRANSLATION_WARNINGS and/or
                 SHOW_LOCALIZATION_WARNINGS
    
    Change-Id: I50ef2a623959a1cc53053b446328df8e34cc99e8
    
    aapt: silence these warnings
    
    to show them, set SHOW_MANIFEST_WARNING
    
    Change-Id: I8fd3617e03ca535df1ae0ebdb3dde3a67eb2737b
    
    aapt: i hate if (true) statements
    
    Change-Id: If0d39f4d942067ae2d4ee4d32490b6e079a5f60d
    
    aapt: we dont care about uncommented public symbols
    
    export SHOW_UNCOMMENTED_SYMBOL_WARNING to show them
    
    Change-Id: I6e6461b54e5cd3a9c09efeb8df3172e48bbfa8e5
    
    aapt: introduce SHOW_EXTENDED_WARNINGS
    
    a quick switch for enabling various warnings we silenced
    
    export SHOW_EXTENDED_WARNINGS to show all silenced warnings
    
    Change-Id: I298f36469d78e335a461f5ccfc2a66aa2976ceac

--- tools/aapt/ResourceTable.cpp
+++ tools/aapt/ResourceTable.cpp
@@ -2810,6 +2810,7 @@ ResourceTable::validateLocalizations(void)
     for (const auto& nameIter : mLocalizations) {
         const std::map<String8, SourcePos>& configSrcMap = nameIter.second;
 
+#ifdef SHOW_DEFAULT_TRANSLATION_WARNINGS
         // Look for strings with no default localization
         if (configSrcMap.count(defaultLocale) == 0) {
             SourcePos().warning("string '%s' has no default translation.",
@@ -2821,7 +2822,8 @@ ResourceTable::validateLocalizations(void)
             }
             // !!! TODO: throw an error here in some circumstances
         }
-
+#endif
+#ifdef SHOW_LOCALIZATION_WARNINGS
         // Check that all requested localizations are present for this string
         if (mBundle->getConfigurations().size() > 0 && mBundle->getRequireLocalization()) {
             const char* allConfigs = mBundle->getConfigurations().string();
@@ -2871,6 +2873,7 @@ ResourceTable::validateLocalizations(void)
                         configStr.string());
             }
         }
+#endif
     }
 
     return err;
